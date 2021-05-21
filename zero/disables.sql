d75e7a03bee58727e381217e588c2cf7705d1e61
Patrick Lewis <Patrick.Lewis@modusbox.com>
2021-05-21 14:39:03 -0700

    feat(ZERO): initial dungeon/raid disables

---
+++ zero/disables.sql
@@ -0,0 +1,36 @@
+-- 40-49 level range
+DELETE FROM `disables` WHERE `entry` IN (109, 209, 349);
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES 
+(2, 109, 1, "", "", "Sunken Temple Dungeon"),
+(2, 209, 1, "", "", "Zul'Farrak (ZF)"),
+(2, 349, 1, "", "", "Maraudon - All wings");
+
+-- 50-59 level range
+DELETE FROM `disables` WHERE `entry` IN (230, 429, 229, 289, 329);
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES 
+(2, 230, 1, "", "", "Blackrock Depths (BRD)"),
+(2, 429, 1, "", "", "Dire Maul - All wings"),
+(2, 229, 1, "", "", "Blackrock Spire - Both Lower (LBRS) & Upper (UBRS)"),
+(2, 289, 1, "", "", "Scholomance"),
+(2, 329, 1, "", "", "Stratholme");
+
+-- 60 level range - Tier 1
+DELETE FROM `disables` WHERE `entry` = 409;
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES 
+(2, 409, 1, "", "", "Molten Core");
+
+-- 60 level range - Tier 2
+DELETE FROM `disables` WHERE `entry` = 469;
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES 
+(2, 469, 1, "", "", "Blackwing Lair");
+
+-- 60 level range - Zul’Gurub
+DELETE FROM `disables` WHERE `entry` = 309;
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES 
+(2, 309, 1, "", "", "Zul’Gurub");
+
+-- 60 level range - Ahn’Qiraj
+DELETE FROM `disables` WHERE `entry` IN (509, 531);
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES 
+(2, 509, 1, "", "", "Ahn’Qiraj Runins"),
+(2, 531, 1, "", "", "Ahn’Qiraj Temple");
