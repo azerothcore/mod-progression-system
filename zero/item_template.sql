0fd9e29b36fbf85499f0ee25b30ce3cfe67935cc
Revision <tkn963@gmail.com>
2021-06-02 17:45:03 +0200

    Fix
    Fixed the mounts that cost honor points. I totally forgot to move these to their own category since they do not have a gold price

--- zero/item_template.sql
+++ zero/item_template.sql
@@ -32,7 +32,7 @@ UPDATE `item_template` SET `spellid_1`=64657 WHERE `entry`=46100; -- White Kodo
 UPDATE `item_template` SET `spellid_1`=64977 WHERE `entry`=46308; -- Black Skeletal Horse
 UPDATE `item_template` SET `spellid_1`=66847 WHERE `entry`=47100; -- Reins of the Striped Dawnsaber
 -- Journeyman
-UPDATE `item_template` SET `BuyPrice`=1000000, `SellPrice`=0, `ItemLevel`=60, `RequiredLevel`=60, `spellcharges_1`=0, `spellid_2`=0, `spelltrigger_2`=0, `description`='' WHERE `entry` IN (13086, 13334, 18766, 18767, 18772, 18773, 18774, 18776, 18777, 18778, 18785, 18786, 18787, 18788, 18789, 18790, 18791, 18793, 18794, 18795, 18796, 18797, 18798, 18902, 19029, 19030, 29465, 29466, 29467, 29468, 29469, 29470, 29471, 29472, 33977, 34129, 35906, 47101);
+UPDATE `item_template` SET `BuyPrice`=1000000, `SellPrice`=0, `ItemLevel`=60, `RequiredLevel`=60, `spellcharges_1`=0, `spellid_2`=0, `spelltrigger_2`=0, `description`='' WHERE `entry` IN (13086, 13334, 18766, 18767, 18772, 18773, 18774, 18776, 18777, 18778, 18785, 18786, 18787, 18788, 18789, 18790, 18791, 18793, 18794, 18795, 18796, 18797, 18798, 18902, 33977, 47101);
 UPDATE `item_template` SET `spellid_1`=17229 WHERE `entry`=13086; -- Reins of the Winterspring Frostsaber
 UPDATE `item_template` SET `spellid_1`=17465 WHERE `entry`=13334; -- Green Skeletal Warhorse
 UPDATE `item_template` SET `spellid_1`=23221 WHERE `entry`=18766; -- Reins of the Swift Frostsaber
@@ -57,6 +57,10 @@ UPDATE `item_template` SET `spellid_1`=23250 WHERE `entry`=18796; -- Horn of the
 UPDATE `item_template` SET `spellid_1`=23251 WHERE `entry`=18797; -- Horn of the Swift Timber Wolf
 UPDATE `item_template` SET `spellid_1`=23252 WHERE `entry`=18798; -- Horn of the Swift Gray Wolf
 UPDATE `item_template` SET `spellid_1`=23338 WHERE `entry`=18902; -- Reins of the Swift Stormsaber
+UPDATE `item_template` SET `spellid_1`=43900 WHERE `entry`=33977; -- Swift Brewfest Ram
+UPDATE `item_template` SET `spellid_1`=66846 WHERE `entry`=47101; -- Ochre Skeletal Warhorse
+
+UPDATE `item_template` SET `ItemLevel`=60, `RequiredLevel`=60, `spellcharges_1`=0, `spellid_2`=0, `spelltrigger_2`=0, `description`='' WHERE `entry` IN (19029, 19030, 29465, 29466, 29467, 29468, 29469, 29470, 29471, 29472, 34129, 35906);
 UPDATE `item_template` SET `spellid_1`=23509 WHERE `entry`=19029; -- Horn of the Frostwolf Howler
 UPDATE `item_template` SET `spellid_1`=23510 WHERE `entry`=19030; -- Stormpike Battle Charger
 UPDATE `item_template` SET `spellid_1`=22719 WHERE `entry`=29465; -- Black Battlestrider
@@ -67,7 +71,5 @@ UPDATE `item_template` SET `spellid_1`=22724 WHERE `entry`=29469; -- Horn of the
 UPDATE `item_template` SET `spellid_1`=22722 WHERE `entry`=29470; -- Red Skeletal Warhorse
 UPDATE `item_template` SET `spellid_1`=22723 WHERE `entry`=29471; -- Reins of the Black War Tiger
 UPDATE `item_template` SET `spellid_1`=22721 WHERE `entry`=29472; -- Whistle of the Black War Raptor
-UPDATE `item_template` SET `spellid_1`=43900 WHERE `entry`=33977; -- Swift Brewfest Ram
 UPDATE `item_template` SET `spellid_1`=35028 WHERE `entry`=34129; -- Swift Warstrider
 UPDATE `item_template` SET `spellid_1`=48027 WHERE `entry`=35906; -- Reins of the Black War Elekk
-UPDATE `item_template` SET `spellid_1`=66846 WHERE `entry`=47101; -- Ochre Skeletal Warhorse
