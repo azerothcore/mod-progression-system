fa5754c7fac5dc106b7834251d8e7ed591f7203b
55Honey <71938210+55Honey@users.noreply.github.com>
2021-08-10 20:03:05 +0200

    chore(disables): move magister's terrace to T5 #2

--- 70/disables.sql
+++ 70/disables.sql
@@ -2,7 +2,7 @@
 DELETE FROM `disables` WHERE `entry` IN (532, 544, 565);
 
 -- 70 level range - Tier 5
-DELETE FROM `disables` WHERE `entry` IN (548, 550);
+DELETE FROM `disables` WHERE `entry` IN (548, 550, 585);
 
 -- 70 level range - Tier 6
 DELETE FROM `disables` WHERE `entry` IN (534, 564, 568, 580);
