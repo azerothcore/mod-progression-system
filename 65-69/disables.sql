0fc0e8b47eacf451ea7d3386622709b5ab033979
55Honey <71938210+55Honey@users.noreply.github.com>
2021-08-10 22:47:04 +0200

    chore(disables): Split T6 and move Magister's Terrace (#32)
    * chore(disables): move magister's terrace to T5
    
    * chore(disables): move magister's terrace to T5 #2
    
    * chore(disables): split T6

--- 65-69/disables.sql
+++ 65-69/disables.sql
@@ -1,2 +1,2 @@
 -- 65-69 level range
-DELETE FROM `disables` WHERE `entry` IN (269, 540, 545, 552, 553, 554, 555, 556, 585);
+DELETE FROM `disables` WHERE `entry` IN (269, 540, 545, 552, 553, 554, 555, 556);
--- 70/disables.sql
+++ 70/disables.sql
@@ -4,5 +4,11 @@ DELETE FROM `disables` WHERE `entry` IN (532, 544, 565);
 -- 70 level range - Tier 5
 DELETE FROM `disables` WHERE `entry` IN (548, 550);
 
--- 70 level range - Tier 6
-DELETE FROM `disables` WHERE `entry` IN (534, 564, 568, 580);
+-- 70 level range - Tier 6 (Mount Hyjal)
+DELETE FROM `disables` WHERE `entry` IN (534);
+
+-- 70 level range - Tier 6 (Black Temple + Magister's Terrace)
+DELETE FROM `disables` WHERE `entry` IN (564, 585);
+
+-- 70 level range - Tier 6 (Sunwell Plateu + Zul Aman)
+DELETE FROM `disables` WHERE `entry` IN (568, 580);
