8b0c224969668fc3f21c74c91f104ebdc6459288
55Honey <71938210+55Honey@users.noreply.github.com>
2021-08-25 22:38:48 +0200

    chore(disables): split 50/59 bracket (#35)

--- 50-59/disables.sql
+++ 50-59/disables.sql
@@ -1,2 +1,5 @@
--- 50-59 level range
-DELETE FROM `disables` WHERE `entry` IN (230, 429, 229, 289, 329);
+-- 50-59 level range (BRD, Dire Maul, Scholomance, Stratholme)
+DELETE FROM `disables` WHERE `entry` IN (230, 429, 289, 329);
+
+-- 50-59 level range (LBRS / UBRS)
+DELETE FROM `disables` WHERE `entry` IN (229);
