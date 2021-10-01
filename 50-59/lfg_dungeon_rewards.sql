3fe7b756d43c2860c4e56cd595ad0cfba8db9f56
Francesco Borzì <borzifrancesco@gmail.com>
2021-10-01 18:28:05 +0200

    Update lfg_dungeon_rewards.sql

--- 50-59/lfg_dungeon_rewards.sql
+++ 50-59/lfg_dungeon_rewards.sql
@@ -2,4 +2,3 @@
 REPLACE INTO `lfg_dungeon_rewards` (`dungeonId`, `maxLevel`, `firstQuestId`, `otherQuestId`)
 VALUES
 (259, 64, 24886, 24894);
-diff --git a/61-64/lfg_dungeon_rewards.sql b/61-64/lfg_dungeon_rewards.sql
