636171278582df3228020e5f166792cc4650e39d
Hacki95 <62942638+Hacki95@users.noreply.github.com>
2021-10-17 11:15:39 +0200

    Update RDF Satchels for 50-60 to only drop gold (#49)

--- 61-64/item_loot_template.sql
+++ 61-64/item_loot_template.sql
@@ -9,3 +9,5 @@ INSERT INTO `item_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Quest
 (52003, 2, 10051, 100, 0, 1, 2, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
 (52003, 3, 10052, 100, 0, 1, 3, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
 (52003, 4, 10053, 100, 0, 1, 4, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)');
+
+UPDATE `item_template` SET `minMoneyLoot` = 0, `maxMoneyLoot` = 0 WHERE `Entry` IN (52002, 52003);
