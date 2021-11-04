bbb9f611bcde37bc13ce5265175def636a8acb6e
Stefano Borzì <stefanoborzi32@gmail.com>
2021-11-04 23:04:05 +0100

    revert: last commit about enabling the quest 8283, it is already enabled by default

--- 50-59/disables.sql
+++ 50-59/disables.sql
@@ -1,5 +1,2 @@
 -- 50-59 level range (BRD, Dire Maul, Scholomance, Stratholme)
 DELETE FROM `disables` WHERE `entry` IN (230, 429, 289, 329);
-
--- Quest: Wanted - Deathclasp, Terror of the Sands
-DELETE FROM `disables` WHERE `sourceType`=1 AND `entry`=8283;
