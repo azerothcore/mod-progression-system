b98150faf877d9e4a4b09f3eaf1ce95ff9d1ef6c
temperrr <temperrr@users.noreply.github.com>
2021-11-08 10:56:40 +0100

    Restore  rdf satchel 49 bracket
    restores the loot of 49 bracket satchel, that was previous obtainable but now would only reward gold.

--- 50-59/custom_rdf_satchels.sql
+++ 50-59/custom_rdf_satchels.sql
@@ -1,5 +1,13 @@
-DELETE FROM `reference_loot_template` WHERE `Entry` IN (999555, 999556);
-DELETE FROM `item_loot_template` WHERE `Entry` IN (52002, 52003);
 
-UPDATE `item_template` SET `minMoneyLoot` = 8000, `maxMoneyLoot` = 25000 WHERE `Entry` IN (52002, 52003);
+DELETE FROM `item_loot_template` WHERE `Entry` IN (52002, 52003);
+UPDATE `item_template` SET `minMoneyLoot` = 8000, `maxMoneyLoot` = 25000 WHERE `Entry` IN  52003);
+
+--Restores previous satchel that was changed to gold reward, back to normal rewards
+INSERT INTO item_loot_template (Entry, Item, Reference, Chance, QuestRequired, LootMode, GroupId, MinCount, MaxCount, Comment) VALUES
+(52002, 1, 10046, 100, 0, 1, 1, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
+(52002, 2, 10047, 100, 0, 1, 2, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
+(52002, 3, 10048, 100, 0, 1, 3, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
+(52002, 4, 10049, 100, 0, 1, 4, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)');
+
+UPDATE item_template SET minMoneyLoot = 0, maxMoneyLoot = 0 WHERE Entry IN (52002);
 
