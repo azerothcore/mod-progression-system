c1a2a6d6e97b01e1909c068566ca79f9dfb876c4
temperrr <temperrr@users.noreply.github.com>
2021-11-08 11:17:26 +0100

    Update custom_rdf_satchels.sql

--- 50-59/custom_rdf_satchels.sql
+++ 50-59/custom_rdf_satchels.sql
@@ -1,4 +1,4 @@
-DELETE FROM `reference_loot_template` WHERE `Entry` IN (999555, 999556);
+DELETE FROM `reference_loot_template` WHERE `Entry` IN (999556);
 DELETE FROM `item_loot_template` WHERE `Entry` IN (52003);
 UPDATE `item_template` SET `minMoneyLoot` = 8000, `maxMoneyLoot` = 25000 WHERE `Entry` IN  52003);
 
