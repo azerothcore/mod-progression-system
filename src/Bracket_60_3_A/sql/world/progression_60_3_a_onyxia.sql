6b58941efc03472d592d066327284b4a916c7739
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2021-11-28 14:41:13 -0300

    fix(MOD): Implement vanilla version of Onyxia

---
+++ src/Bracket_60_3_A/sql/world/progression_60_3_a_onyxia.sql
@@ -0,0 +1,2 @@
+-- Onyxia
+UPDATE `creature_template` SET `minlevel` = 63, `maxlevel` = 63 WHERE `entry` = 10184;
---
+++ src/Bracket_80_7/sql/world/progression_80_7_onyxia_restore.sql
@@ -0,0 +1,2 @@
+-- Onyxia
+UPDATE `creature_template` SET `minlevel` = 83, `maxlevel` = 83 WHERE `entry` = 10184;
