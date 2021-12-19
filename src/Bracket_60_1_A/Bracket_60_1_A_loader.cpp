c2d187d18de4a3f1d28baeb37d38ca56da6d6f5d
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2021-12-19 17:03:14 -0300

    Implement support for manual handling of Firelord Runes (related to Poisoned Water quest chain) (#96)
    * Implement molten core custom scripts
    
    * Spawn majordomo when all runes are doused
    
    * db misc yadda
    
    * config

--- src/Bracket_60_1_A/Bracket_60_1_A_loader.cpp
+++ src/Bracket_60_1_A/Bracket_60_1_A_loader.cpp
@@ -4,8 +4,12 @@
 
 #include "ProgressionSystem.h"
 
+void AddSC_instance_molten_core_60_1_A();
+
 void AddBracket_60_1_A_Scripts()
 {
     if (!(sConfigMgr->GetOption<int>("ProgressionSystem.Brackets", 0) & PROGRESSION_BRACKET_60_TIER_1_A))
         return;
+
+    AddSC_instance_molten_core_60_1_A();
 }
