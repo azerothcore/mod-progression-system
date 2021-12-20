2ccf20e8a78b8debfb6709db5c68dca4490497c1
temperrr <temperrr@users.noreply.github.com>
2021-12-20 22:15:53 +0100

    Create progression_60_1_a_Apply_mc_hp.sql
    Draft for hp tuning mc

---
+++ src/Bracket_60_1_A/sql/world/progression_60_1_a_Apply_mc_hp.sql
@@ -0,0 +1,57 @@
+-- Increasing MC boss hp by 3x
+
+-- Lucifron 132 - 396
+UPDATE `creature_template` SET `HealthModifier` = 396 WHERE (`entry` = 12118);
+
+-- Magmadar 248 - 744
+UPDATE `creature_template` SET `HealthModifier` = 744 WHERE (`entry` = 11982);
+
+-- Gehennas 132 - 396
+UPDATE `creature_template` SET `HealthModifier` = 396 WHERE (`entry` = 12259);
+
+-- Garr 198 - 594
+UPDATE `creature_template` SET `HealthModifier` = 594 WHERE (`entry` = 12057);
+
+-- baron Geddon 176 - 528
+UPDATE `creature_template` SET `HealthModifier` = 528 WHERE (`entry` = 12056);
+
+-- Shazzrah 132 - 396
+UPDATE `creature_template` SET `HealthModifier` = 396 WHERE (`entry` = 12264);
+
+-- Golemagg the incinerator 248 - 744
+UPDATE `creature_template` SET `HealthModifier` = 744 WHERE (`entry` = 11988);
+
+-- sulfuron harbinger 132 - 396
+UPDATE `creature_template` SET `HealthModifier` = 396 WHERE (`entry` = 12098);
+
+-- majordomo executus 200-600
+UPDATE `creature_template` SET `HealthModifier` = 600 WHERE (`entry` = 12018);
+
+-- Ragnaros 330 - 990
+UPDATE `creature_template` SET `HealthModifier` = 990 WHERE (`entry` = 11502);
+
+-- Increasing MCAdd hp by 2.5x
+
+-- Flamewaker Protector 35 - 87.5
+UPDATE `creature_template` SET `HealthModifier` = 87.5 WHERE (`entry` = 12119);
+
+-- Flamewaker 30 -  75
+UPDATE `creature_template` SET `HealthModifier` = 75 WHERE (`entry` = 11661);
+
+-- Firesworn 20 - 50
+UPDATE `creature_template` SET `HealthModifier` = 50 WHERE (`entry` = 12099);
+
+-- Core Rager 25 - 62.5
+UPDATE `creature_template` SET `HealthModifier` = 62.5 WHERE (`entry` = 11672);
+
+-- Flamewaker Priest 30 - 75
+UPDATE `creature_template` SET `HealthModifier` = 75 WHERE (`entry` = 11662);
+
+-- Flamewaker Healer 40 - 100
+UPDATE `creature_template` SET `HealthModifier` = 100 WHERE (`entry` = 11663);
+
+-- Flamewaker Elite 40 - 100
+UPDATE `creature_template` SET `HealthModifier` = 100 WHERE (`entry` = 11664);
+
+-- Son of Flame 5 - 12.5
+UPDATE `creature_template` SET `HealthModifier` = 12.5 WHERE (`entry` = 12143);
