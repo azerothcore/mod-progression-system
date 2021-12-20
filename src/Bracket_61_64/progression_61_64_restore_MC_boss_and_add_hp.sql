c34ff9e815d7a014111dc8bb7b03b59d67cdae51
temperrr <temperrr@users.noreply.github.com>
2021-12-20 22:17:29 +0100

    Create progression_61_64_restore_MC_boss_and_add_hp.sql

---
+++ src/Bracket_61_64/progression_61_64_restore_MC_boss_and_add_hp.sql
@@ -0,0 +1,58 @@
+-- RESTORE mc boss hp
+
+-- Lucifron
+UPDATE `creature_template` SET `HealthModifier` = 132 WHERE (`entry` = 12118);
+
+-- Magmadar
+UPDATE `creature_template` SET `HealthModifier` = 248 WHERE (`entry` = 11982);
+
+-- Gehennas
+UPDATE `creature_template` SET `HealthModifier` = 132 WHERE (`entry` = 12259);
+
+-- Garr
+UPDATE `creature_template` SET `HealthModifier` = 198 WHERE (`entry` = 12057);
+
+-- baron Geddon
+UPDATE `creature_template` SET `HealthModifier` = 176 WHERE (`entry` = 12056);
+
+-- Shazzrah
+UPDATE `creature_template` SET `HealthModifier` = 132 WHERE (`entry` = 12264);
+
+-- golemagg the incinerator
+UPDATE `creature_template` SET `HealthModifier` = 248 WHERE (`entry` = 11988);
+
+-- sulfuron harbinger
+UPDATE `creature_template` SET `HealthModifier` = 132 WHERE (`entry` = 12098);
+
+-- majordomo executus
+UPDATE `creature_template` SET `HealthModifier` = 200 WHERE (`entry` = 12018);
+
+-- Ragnaros
+UPDATE `creature_template` SET `HealthModifier` = 330 WHERE (`entry` = 11502);
+
+-- Restoring Boss add hp
+
+-- Flamewaker Protector
+UPDATE `creature_template` SET `HealthModifier` = 35 WHERE (`entry` = 12119);
+
+-- Flamewaker
+UPDATE `creature_template` SET `HealthModifier` = 30 WHERE (`entry` = 11661);
+
+-- Firesworn
+UPDATE `creature_template` SET `HealthModifier` = 20 WHERE (`entry` = 12099);
+
+-- Core Rager
+UPDATE `creature_template` SET `HealthModifier` = 25 WHERE (`entry` = 11672);
+
+-- Flamewaker Priest
+UPDATE `creature_template` SET `HealthModifier` = 30 WHERE (`entry` = 11662);
+
+-- Flamewaker Healer
+UPDATE `creature_template` SET `HealthModifier` = 40 WHERE (`entry` = 11663);
+
+-- Flamewaker Elite
+UPDATE `creature_template` SET `HealthModifier` = 40 WHERE (`entry` = 11664);
+
+-- Son of Flame
+UPDATE `creature_template` SET `HealthModifier` = 5 WHERE (`entry` = 12143);
+
