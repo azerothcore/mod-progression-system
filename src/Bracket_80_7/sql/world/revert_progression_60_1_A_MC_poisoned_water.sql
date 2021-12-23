956bafeba89b7fb4e922d502d3ec189328638bd9
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2021-12-22 23:11:05 -0300

    Fix Poisoned Water conditions

--- src/Bracket_80_7/sql/world/revert_progression_60_1_A_MC_poisoned_water.sql
+++ src/Bracket_80_7/sql/world/revert_progression_60_1_A_MC_poisoned_water.sql
@@ -1,3 +1,4 @@
-DELETE FROM `creature_queststarter` WHERE `quest` IN (6821, 6822, 6823, 6824, 7486) AND `id` = 13278;
-DELETE FROM `creature_questender` WHERE `quest` IN (6821, 6822, 6823, 6824) AND `id` = 13278;
+DELETE FROM `creature_queststarter` WHERE `quest` IN (6804, 6821, 6822, 6823, 6824, 7486) AND `id` = 13278;
+DELETE FROM `creature_questender` WHERE `quest` IN (6804, 6821, 6822, 6823, 6824) AND `id` = 13278;
 DELETE FROM `quest_template_addon` WHERE `ID` IN (6821, 6822, 6823, 6824, 7486);
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 19 AND `SourceEntry` = 6821;
