877c490599a7f11151f51c411a65a3adef56b831
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-01-16 15:11:05 -0300

    katrana SAI

--- src/Bracket_60_2/sql/world/progression_60_2_the_masquerade.sql
+++ src/Bracket_60_2/sql/world/progression_60_2_the_masquerade.sql
@@ -239,4 +239,11 @@ INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionTex
 
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 59563;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-('15','59563','0','0','0','9','0','4224','0','0','0','0','0','','Ragged John - Require True Masters (6)');
+(15,59563,0,0,0,9,0,4224,0,0,0,0,0,'','Ragged John - Require True Masters (6)');
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 1749;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 1749 AND `Source_type` = 0; 
+insert into `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) values
+(1749,0,0,1,62,0,100,0,51752,0,0,0,0,72,0,0,0,0,0,0,7,0,0,0,0,0,0,0,'Lady Katrana Prestor - On Gossip Option Select - Close Gossip'),
+(1749,0,1,0,61,0,100,0,0,0,0,0,0,15,4185,0,0,0,0,0,7,0,0,0,0,0,0,0,'Lady Katrana Prestor - On Gossip Option Select - Complete Quest - The True Masters');
