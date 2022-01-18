8659e34be9f2aa9e0498e6dcedf17d0b73076662
temperrr <temperrr@users.noreply.github.com>
2022-01-18 13:08:39 +0100

    Satchel 50 loot restore and satchel 55 will contain 50 loot (#55)
    * Restore  rdf satchel 49 bracket
    
    restores the loot of 49 bracket satchel, that was previous obtainable but now would only reward gold.
    
    * Update custom_rdf_satchels.sql
    
    * Update item_loot_template.sql
    
    * Update custom_rdf_satchels.sql
    
    * Update custom_rdf_satchels.sql
    
    * Update custom_rdf_satchels.sql
    
    * Delete custom_rdf_satchels.sql
    
    * Update item_loot_template.sql
    
    * Update item_loot_template.sql
    
    * 52003 loot same as 52002 loot

--- src/Bracket_61_64/sql/world/progression_61_64_item_loot_template.sql
+++ src/Bracket_61_64/sql/world/progression_61_64_item_loot_template.sql
@@ -1,13 +1,10 @@
--- restore Satchel of Helpful Goods loot (50, 57)
-DELETE FROM `item_loot_template` WHERE `Entry` IN (52002, 52003);
+
+-- restore Satchel of Helpful Goods loot (55)
+DELETE FROM `item_loot_template` WHERE `Entry` IN (52003);
 INSERT INTO `item_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(52002, 1, 10046, 100, 0, 1, 1, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
-(52002, 2, 10047, 100, 0, 1, 2, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
-(52002, 3, 10048, 100, 0, 1, 3, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
-(52002, 4, 10049, 100, 0, 1, 4, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
+
 (52003, 1, 10050, 100, 0, 1, 1, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
 (52003, 2, 10051, 100, 0, 1, 2, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
 (52003, 3, 10052, 100, 0, 1, 3, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)'),
 (52003, 4, 10053, 100, 0, 1, 4, 1, 1, 'Satchel of Helpful Goods - (ReferenceTable)');
 
-UPDATE `item_template` SET `minMoneyLoot` = 0, `maxMoneyLoot` = 0 WHERE `Entry` IN (52002, 52003);
