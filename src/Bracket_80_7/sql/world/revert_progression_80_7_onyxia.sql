28241d2383791821ce758f68ea9ca026ab61eae5
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-01-26 15:47:01 -0300

    chore: remove skin loot too (#130)
    * fix: Onyxia25 tuning (trash and damage)
    
    * chore: Remove skin loot too

--- src/Bracket_80_7/sql/world/revert_progression_80_7_onyxia.sql
+++ src/Bracket_80_7/sql/world/revert_progression_80_7_onyxia.sql
@@ -1,8 +1,7 @@
 -- Onyxia
 UPDATE `creature_template` SET `minlevel` = 83, `maxlevel` = 83, `lootid` = 36538, `damagemodifier` = 70 WHERE `entry` = 36538;
 
-UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `lootid` = 12129 WHERE `entry` = 36572;
-
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `lootid` = 12129, `skinloot` = 12129 WHERE `entry` = 36572;
 
 DELETE FROM `creature_loot_template` WHERE `Entry` = 54003;
 DELETE FROM `reference_loot_template` WHERE `Entry` IN (54001, 54002);
