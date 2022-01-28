517d271d0e2dec5ccec8524df1633fa3a53eb2fe
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-01-28 12:52:38 -0300

    fix: Replace the 25 player version with the 10 player one (#133)
    * fix: Replace the 25 player version with the 10 player one
    
    * fiix loot

--- src/Bracket_60_2/sql/world/progression_60_2_onyxia.sql
+++ src/Bracket_60_2/sql/world/progression_60_2_onyxia.sql
@@ -1,8 +1,8 @@
 -- Onyxia
-UPDATE `creature_template` SET `minlevel` = 63, `maxlevel` = 63, `damagemodifier` = 35 WHERE `entry` = 36538;
+UPDATE `creature_template` SET `minlevel` = 63, `maxlevel` = 63, `lootid` = 54003 WHERE `entry` = 10184;
 
 -- Trash mobs
-UPDATE `creature_template` SET `minlevel` = 60, `maxlevel` = 60, `lootid` = 0, `skinloot` = 0 WHERE `entry` = 36572;
+UPDATE `creature_template` SET `minlevel` = 60, `maxlevel` = 60, `lootid` = 0, `skinloot` = 0 WHERE `entry` IN (11262, 12129);
 
 DELETE FROM `reference_loot_template` WHERE `Entry` IN (54000, 54001);
 INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
@@ -35,8 +35,20 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 (54003, 18705, 0, 100, 1, 1, 0, 1, 1, 'Onyxia - Mature Black Dragon Sinew'),
 (54003, 21108, 0, 100, 0, 1, 0, 1, 1, 'Onyxia - Draconic for Dummies');
 
-UPDATE `creature_template` SET `lootid` = 54003 WHERE `entry` = 36538;
-
-DELETE FROM `disables` WHERE `entry` = 12559 AND `sourceType` = 4;
+DELETE FROM `disables` WHERE `entry` IN (12565, 12566, 12567, 12558) AND `sourceType` = 4;
 INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES
-(4, 12559, 0, 0, 0, 'Disable Onyxia Lair 25 achievement');
+(4, 12565, 0, 0, 0, 'Disable Many Whelps, Handle it! (10) achievement'),
+(4, 12566, 0, 0, 0, 'Disable She deeps breah more (10) achievement'),
+(4, 12564, 0, 0, 0, 'Disable More dots! (10) achievement'),
+(4, 12558, 0, 0, 0, 'Disable Onyxia Lair (10) achievement');
+
+DELETE FROM `dungeon_access_requirements` WHERE `dungeon_access_id` = 15;
+INSERT INTO `dungeon_access_requirements` (`dungeon_access_id`, `requirement_type`, `requirement_id`, `requirement_note`, `faction`, `priority`, `leader_only`, `comment`) VALUES
+(15, 2, 16309, 'You must have the Drakefire Amulet in your inventory before entering Onyxia\'s Lair.', 2, NULL, 0, '');
+
+UPDATE `dungeon_access_template` SET `min_level` = 60 WHERE `map_id` = 249 AND `difficulty` = 0;
+
+DELETE FROM `mapdifficulty_dbc` WHERE `MapID` = 249;
+INSERT INTO `mapdifficulty_dbc` (`MapID`, `Difficulty`, `MaxPlayers`) VALUES
+(249, 0, 25);
+
--- src/Bracket_80_7/sql/world/revert_progression_80_7_onyxia.sql
+++ src/Bracket_80_7/sql/world/revert_progression_80_7_onyxia.sql
@@ -1,9 +1,17 @@
 -- Onyxia
-UPDATE `creature_template` SET `minlevel` = 83, `maxlevel` = 83, `lootid` = 36538, `damagemodifier` = 70 WHERE `entry` = 36538;
+UPDATE `creature_template` SET `minlevel` = 83, `maxlevel` = 83, `lootid` = 10184 WHERE `entry` = 10184;
 
-UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `lootid` = 12129, `skinloot` = 12129 WHERE `entry` = 36572;
+UPDATE `creature_template` SET `minlevel` = 60, `maxlevel` = 60, `lootid` = 0, `skinloot` = 0 WHERE `entry` IN (11262, 12129);
 
 DELETE FROM `creature_loot_template` WHERE `Entry` = 54003;
 DELETE FROM `reference_loot_template` WHERE `Entry` IN (54001, 54002);
 
 DELETE FROM `disables` WHERE `entry` = 12559 AND `sourceType` = 4;
+
+DELETE FROM `disables` WHERE `entry` IN (12565, 12566, 12567, 12558) AND `sourceType` = 4;
+
+DELETE FROM `dungeon_access_requirements` WHERE `dungeon_access_id` = 15;
+
+UPDATE `dungeon_access_template` SET `min_level` = 80 WHERE `map_id` = 249 AND `difficulty` = 0;
+
+DELETE FROM `mapdifficulty_dbc` WHERE `MapID` = 249;
