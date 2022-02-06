786d192280ea47129b27874459b26660bf9bcec7
Hacki95 <daniel.hackmann@stud.hs-bochum.de>
2022-02-06 13:13:43 +0100

    Fix more id issues

--- src/Bracket_80_10_A/sql/world/progression_80_10_a_creature.sql
+++ src/Bracket_80_10_A/sql/world/progression_80_10_a_creature.sql
@@ -1,3 +1,3 @@
 -- 80 level range - Tier 10 & Wrathful Gladiator
 -- restore Toravon at Vaulth of Archavon
-UPDATE `creature` SET `phaseMask` = 1 WHERE `id` = 38433;
+UPDATE `creature` SET `phaseMask` = 1 WHERE `id1` = 38433;
--- src/Bracket_80_9/sql/world/progression_80_9_creature.sql
+++ src/Bracket_80_9/sql/world/progression_80_9_creature.sql
@@ -1,3 +1,3 @@
 -- 80 level range - Tier 9 & Relentless Gladiator
 -- restore Koralon at Vaulth of Archavon
-UPDATE `creature` SET `phaseMask` = 1 WHERE `id` = 35013;
+UPDATE `creature` SET `phaseMask` = 1 WHERE `id1` = 35013;
