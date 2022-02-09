185a42654cf509b46889c80b3bad1ea58a00999c
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-02-09 11:05:02 -0300

    feat(Events): Implement downscalled love's in the air (#156)

--- src/Bracket_1_19/Bracket_1_19_loader.cpp
+++ src/Bracket_1_19/Bracket_1_19_loader.cpp
@@ -4,8 +4,12 @@
 
 #include "ProgressionSystem.h"
 
+void AddSC_event_love_in_the_air_1_19();
+
 void AddBracket_1_19_Scripts()
 {
     if (!(sConfigMgr->GetOption<int>("ProgressionSystem.Brackets", 0) & PROGRESSION_BRACKET_1_19))
         return;
+
+    AddSC_event_love_in_the_air_1_19();
 }
