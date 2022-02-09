24de379920d097666a0dab71d668627c251d5895
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-02-09 10:40:52 -0300

    fix: Kazzak level & Mark of Kazzak

--- src/Bracket_70_4/sql/world/revert_progression_60_2_kazzak.sql
+++ src/Bracket_70_4/sql/world/revert_progression_60_2_kazzak.sql
@@ -1,7 +1,8 @@
-UPDATE `creature_template` SET `ScriptName` = '' WHERE `entry` = 12397;
+UPDATE `creature_template` SET `minlevel` = 73, `maxlevel` = 73, `ScriptName` = '' WHERE `entry` = 12397;
 
 DELETE FROM `spell_script_names` WHERE `spell_id` = 21056;
 
 DELETE FROM `creature_text` WHERE `CreatureID` = 12397;
 
 DELETE FROM `creature` WHERE `guid` = 156950 AND `id1` = 12397;
+
