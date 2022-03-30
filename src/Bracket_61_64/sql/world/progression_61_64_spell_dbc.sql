a9e6c96b6369c21a4e171c7a708646ab1463a920
temperrr <temperrr@users.noreply.github.com>
2022-03-30 10:09:58 +0200

    Move Baron Geddon spell_dbc revert and add BWL revert

---
+++ src/Bracket_61_64/sql/world/progression_61_64_spell_dbc.sql
@@ -0,0 +1,4 @@
+#Revoke change Baron Geddon explosion base damage from 3200 to 2100
+DELETE FROM `spell_dbc` WHERE `ID` = 20476;
+#Revoke changes in BWL
+DELETE FROM `spell_dbc` WHERE `ID` IN (23331, 24573, 22539);
