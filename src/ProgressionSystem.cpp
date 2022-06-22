5408b19203f7502432a963c7edb7592ec7454f74
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-06-22 13:11:48 -0300

    feat: Allow reapplying updates on start up

--- src/ProgressionSystem.cpp
+++ src/ProgressionSystem.cpp
@@ -34,6 +34,11 @@ public:
     {
         if (DBUpdater<LoginDatabaseConnection>::IsEnabled(updateFlags))
         {
+            if (sConfigMgr->GetOption<bool>("ProgressionSystem.ReapplyUpdates", false))
+            {
+                LoginDatabase.Query("DELETE FROM updates WHERE name LIKE '%progression%'");
+            }
+
             std::vector<std::string> loginDatabaseDirectories = GetDatabaseDirectories("auth");
             if (!loginDatabaseDirectories.empty())
             {
@@ -43,6 +48,11 @@ public:
 
         if (DBUpdater<CharacterDatabaseConnection>::IsEnabled(updateFlags))
         {
+            if (sConfigMgr->GetOption<bool>("ProgressionSystem.ReapplyUpdates", false))
+            {
+                CharacterDatabase.Query("DELETE FROM updates WHERE name LIKE '%progression%'");
+            }
+
             std::vector<std::string> charactersDatabaseDirectories = GetDatabaseDirectories("characters");
             if (!charactersDatabaseDirectories.empty())
             {
@@ -52,6 +62,11 @@ public:
 
         if (DBUpdater<WorldDatabaseConnection>::IsEnabled(updateFlags))
         {
+            if (sConfigMgr->GetOption<bool>("ProgressionSystem.ReapplyUpdates", false))
+            {
+                WorldDatabase.Query("DELETE FROM updates WHERE name LIKE '%progression%'");
+            }
+
             std::vector<std::string> worldDatabaseDirectories = GetDatabaseDirectories("world");
             if (!worldDatabaseDirectories.empty())
             {
