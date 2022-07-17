ebac1d3d82753dc76b16c8a7eea1bdfa764a832f
temperrr <temperrr@users.noreply.github.com>
2022-07-17 08:18:29 +0200

    Update progression_60_1_mapdifficulty_dbc.sql

--- src/Bracket_60_1/sql/world/progression_60_1_mapdifficulty_dbc.sql
+++ src/Bracket_60_1/sql/world/progression_60_1_mapdifficulty_dbc.sql
@@ -1,6 +1,2 @@
-DELETE FROM `mapdifficulty_dbc` WHERE `ID` IN (24,34,39,44);
-INSERT INTO `mapdifficulty_dbc` (`ID`, `MapID`, `Difficulty`, `RaidDuration`, `MaxPlayers`, `Difficultystring`) VALUES
-(24, 229, 0, 0, 10, ''), #UBRS
-(34, 409, 0, 604800, 20, 'RAID_DIFFICULTY_40PLAYER'), #MC
-(39, 469, 0, 604800, 25, 'RAID_DIFFICULTY_40PLAYER'), #BWL
-(44, 531, 0, 604800, 25, 'RAID_DIFFICULTY_40PLAYER'), #AQ40
+UPDATE `mapdifficulty_dbc` SET `MaxPlayers` = 25 WHERE `ID` IN (34, 39, 44);
+UPDATE `mapdifficulty_dbc` SET `MaxPlayers` = 10 WHERE `ID` IN (24);
