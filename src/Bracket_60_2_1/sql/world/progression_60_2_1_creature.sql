2b048e20671b86d8d36ad19184f614a2b4f1bb27
temperrr <speudoniem@outlook.com>
2022-08-03 14:21:28 +0200

    Update progression_60_2_1_creature.sql

--- src/Bracket_60_2_1/sql/world/progression_60_2_1_creature.sql
+++ src/Bracket_60_2_1/sql/world/progression_60_2_1_creature.sql
@@ -1,2 +1,2 @@
 INSERT INTO `creature` (`guid`, `id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`,  `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES 
-(43175, 15270, 1, 0, 0, 1, 1, 15371, 1, -6838.07, 843.766, 49.0494, 5.44543, 300, 0, 0, 3662, 0, 0, 0, 0, 0, '', 0);
+(43175, 15270, 1, 0, 0, 1, 1, 0, -6838.07, 843.766,  49.0494, 5.44543, 300, 0, 0, 3662, 0, 0, 0, 0, 0, '', 0);
