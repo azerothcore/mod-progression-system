ecc7af246fe9fe49e81cd98a56663a8eed8759d7
Sogla <soglahash@gmail.com>
2022-08-07 16:40:40 +0200

    Set variable for portal location; add meeting stone

--- src/Bracket_60_3_3/sql/world/progression_60_3_3_naxx.sql
+++ src/Bracket_60_3_3/sql/world/progression_60_3_3_naxx.sql
@@ -12,35 +12,78 @@ VALUES
 -- Set access to min level 60 for Naxx25
 UPDATE `dungeon_access_template` SET `min_level` = 60 WHERE `map_id` = @MapID AND `difficulty` = 1;
 
--- Floating Naxxramas object, "naxxramas" id: 181056
+-- .go xyz 3094.8951 -3873.87 138.36
+
+-- Update Graveyard Position from Dragonblight to EPL
+-- https://wow.tools/dbc/?dbc=worldsafelocs&build=3.3.5.12340#page=1&search=909
+-- https://wow.tools/dbc/?dbc=areatable&build=3.3.5.12340#page=1&search=3456
+DELETE FROM `graveyard_zone` WHERE `GhostZone` = 3456;
+INSERT INTO `graveyard_zone` (`ID`, `GhostZone`, `Faction`, `Comment`)
+VALUES
+(909, 3456, 0, 'Naxxramas, Eastern Plaguelands - Blackwood Lake');
+
+-- Set coordinates for Naxx in EPL
+SET @PORTAL_X:= 3094.8951;
+SET @PORTAL_Y:= -3873.87;
+SET @PORTAL_Z:= 135.46;
+SET @PORTAL_O:= 3.2138;
+SET @EXIT_Z:= 138.36;
+
+-- Add Entrance portal object. Green circle (id: 190564)
+DELETE FROM gameobject WHERE id=190564 AND map=0 AND zoneId = 0 AND areaID =0;
+INSERT INTO gameobject
+(id, `map`, zoneId, areaId, spawnMask, phaseMask, position_x, position_y,
+position_z, orientation, rotation0, rotation1, rotation2, rotation3,
+spawntimesecs, animprogress, state, ScriptName, VerifiedBuild)
+VALUES
+(190564, 0, 0, 0, 1, 1, @PORTAL_X, @PORTAL_Y, @PORTAL_Z, @PORTAL_O, 0, 0,
+-0.063658, -1, 1, 0, 1, '', 0);
+
+-- Add Floating Naxx Object (id: 181056)
 -- TODO: Fix visibility. Not always visible
--- DELETE FROM `gameobject_template` WHERE `entry`=181056;
--- INSERT INTO gameobject_template
--- (entry, `type`, displayId, name, IconName, castBarCaption, unk1, `size`, Data0, Data1, Data2, Data3, Data4, Data5, Data6, Data7, Data8, Data9, Data10, Data11, Data12, Data13, Data14, Data15, Data16, Data17, Data18, Data19, Data20, Data21, Data22, Data23, AIName, ScriptName, VerifiedBuild)
--- VALUES(181056, 1, 6637, 'Naxxramas', '', '', '', 1.0, 436, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 1);
--- Spawn floating above cauldron
 DELETE FROM `gameobject` WHERE `id`=181056;
 INSERT INTO `gameobject`
 (`id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`,
 `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`,
 `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`)
 VALUES
-(181056, 0, 0, 0, 1, 1, 3132, -3731, 200, -2.148, 0.0, 0.0, 0, 0, 900, 100, 1, '', 0);
+(181056, 0, 0, 0, 1, 1, @PORTAL_X, @PORTAL_Y, 200, -2.148, 0.0, 0.0, 0, 0, 900,
+100, 1, '', 0);
 
--- Update Naxx exits to somewhere in EPL
-SET @EXIT_X:= 3090.68;
-SET @EXIT_Y:= -3874.88;
-SET @EXIT_Z:= 138.36;
-SET @EXIT_O:= 3.2138;
+-- Update Naxx exits (mapID: 533) to somewhere in EPL
 DELETE FROM `areatrigger_teleport` WHERE `ID` in (5196, 5197, 5198, 5199);
 INSERT INTO `areatrigger_teleport`
-(`ID`, `Name`, `target_map`, `target_position_x`, `target_position_y`, `target_position_z`,
-`target_orientation`)
+(`ID`, `Name`, `target_map`, `target_position_x`, `target_position_y`,
+`target_position_z`, `target_orientation`)
 VALUES
-(5196, 'Naxxramas (exit1)', 0, @EXIT_X, @EXIT_Y, @EXIT_Z, @EXIT_O),
-(5197, 'Naxxramas (exit2)', 0, @EXIT_X, @EXIT_Y, @EXIT_Z, @EXIT_O),
-(5198, 'Naxxramas (exit3)', 0, @EXIT_X, @EXIT_Y, @EXIT_Z, @EXIT_O),
-(5199, 'Naxxramas (exit4)', 0, @EXIT_X, @EXIT_Y, @EXIT_Z, @EXIT_O);
+(5196, 'Naxxramas (exit1)', 0, @PORTAL_X, @PORTAL_Y, @EXIT_Z, @PORTAL_O),
+(5197, 'Naxxramas (exit2)', 0, @PORTAL_X, @PORTAL_Y, @EXIT_Z, @PORTAL_O),
+(5198, 'Naxxramas (exit3)', 0, @PORTAL_X, @PORTAL_Y, @EXIT_Z, @PORTAL_O),
+(5199, 'Naxxramas (exit4)', 0, @PORTAL_X, @PORTAL_Y, @EXIT_Z, @PORTAL_O);
+
+-- Meeting stone
+DELETE FROM `gameobject` WHERE id=193166;
+INSERT INTO `gameobject`
+(`id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`,
+`position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`,
+`rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`,
+`ScriptName`, `VerifiedBuild`) VALUES
+(193166, 0, 0, 0, 1, 1, 3072.64, -3886.67, 130.449, 2.4011, 0.0, 0.0, -0.932239,
+-0.361844, 300, 0, 1, '', 0);
+-- Update meeting stone requirements (previous: minLvl 15)
+-- AreaTable ID: 3456 ContinentID: 533
+-- https://wow.tools/dbc/?dbc=areatable&build=3.3.5.12340#page=1&search=3456
+DELETE FROM `gameobject_template` WHERE entry=193166;
+SET @MINLEVEL:=55;
+INSERT INTO `gameobject_template`
+(`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`,
+`size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `Data6`, `Data7`,
+`Data8`, `Data9`, `Data10`, `Data11`, `Data12`, `Data13`, `Data14`, `Data15`,
+`Data16`, `Data17`, `Data18`, `Data19`, `Data20`, `Data21`, `Data22`, `Data23`,
+`AIName`, `ScriptName`, `VerifiedBuild`)
+VALUES
+(193166, 23, 5492, 'Meeting Stone', '', '', '', 0.75, @MINLEVEL, 255, 3456, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 12340);
 
 -- Disable 25man achievements
 -- https://wow.tools/dbc/?dbc=achievement_criteria&build=3.3.5.12340
@@ -50,8 +93,9 @@ DELETE FROM `disables` WHERE `entry` IN
 7159, 7160, 7161, 7162, 7163, 7164, 7165, 7166, 7167, 7168, 7169, 7170, 7171,
 7172, 7193, 7548, 7549, 7568, 7601, 7603, 7605, 7613, 7615, 7616)
 AND `sourceType` = 4;
-INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`,
-`comment`) VALUES
+INSERT INTO `disables`
+ (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`)
+VALUES
 (4,  7601, 0, '', '', 'Disable And They Would All Go Down Together (25 player)'),
 (4,  7129, 0, '', '', 'Disable Arachnophobia (25 player)'),
 (4,  7615, 0, '', '', 'Disable Just Can\'t Get Enough (25 player)'),
@@ -109,9 +153,7 @@ UPDATE `creature_template` SET `lootid` = 0, `skinloot` = 0 WHERE `Entry` IN
 29837, 29898, 29899, 29900, 29242, 29241, 29243, 29229, 30389, 29286, 29247,
 29613, 29248, 29575, 29574, 30075, 30087, 29347, 29353, 29362, 29359, 29363,
 29355, 29371, 29609, 29608, 29603, 29601, 29612, 31542, 29852, 30424);
--- Disable loot for non-bosses that are spawned
--- already disabled
-
+-- Skip Disable loot for non-bosses that are spawned as they don't drop loot
 -- Disable loot for bosses
 DELETE FROM `creature_loot_template` WHERE `Entry` IN (29373, 29417, 29701,
 29268, 29615, 29249, 29718, 29324, 29955, 29940, 29448, 29278, 30603, 30601,
