f9b5b157aeb64ef73899a871e402b002fa35a258
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-08-15 11:59:30 -0300

    fix: Remove Brood of Nozdormu rep from AQ20 bosses until AQ40 releases and split brackets

---
+++ src/Bracket_60_3_2/sql/world/progression_60_3_2_aq20_brood_rep.sql
@@ -0,0 +1,5 @@
+-- 910, 50
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepFaction1` = 0, `RewOnKillRepValue1`= 0 WHERE `creature_id` IN (15369, 15370, 15341, 15348, 15340);
+
+-- 910, 100
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepFaction1` = 0, `RewOnKillRepValue1`= 0 WHERE `creature_id` = 15339; -- Ossirian
