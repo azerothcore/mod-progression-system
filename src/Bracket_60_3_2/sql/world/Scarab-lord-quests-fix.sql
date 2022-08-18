ad23ba554b2db877533c4d59802600b3b5768962
Maelthyr <100411212+Maelthyrr@users.noreply.github.com>
2022-08-18 22:04:31 +0200

    Update Scarab-lord-quests-fix.sql

--- src/Bracket_60_3_2/sql/world/Scarab-lord-quests-fix.sql
+++ src/Bracket_60_3_2/sql/world/Scarab-lord-quests-fix.sql
@@ -2,7 +2,6 @@
 UPDATE `creature_loot_template` SET `Chance` = 1 WHERE `Item` = 21146;
 UPDATE `creature_loot_template` SET `Chance` = 1 WHERE `Item` = 21147;
 UPDATE `creature_loot_template` SET `Chance` = 1 WHERE `Item` = 21148;
-UPDATE `creature_loot_template` SET `Chance` = 1 WHERE `Item` = 21148;
 
 -- FIx drop rate for Narain's Scrying Goggles
 UPDATE `creature_loot_template` SET `Chance` = 2 WHERE `Item` = 20951;
