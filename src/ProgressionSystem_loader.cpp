0396d0a968c144de2dcde3042ac0b0b5c7b0167f
temperrr <speudoniem@outlook.com>
2022-08-24 15:13:48 +0200

    fix bracket loading (#247)

--- src/ProgressionSystem_loader.cpp
+++ src/ProgressionSystem_loader.cpp
@@ -18,6 +18,7 @@ void AddBracket_60_2_Scripts();
 void AddBracket_60_2_A_Scripts();
 void AddBracket_60_3_A_Scripts();
 void AddBracket_60_3_B_Scripts();
+void AddBracket_60_3_C_Scripts();
 void AddBracket_61_64_Scripts();
 void AddBracket_65_69_Scripts();
 void AddBracket_70_4_Scripts();
@@ -53,6 +54,7 @@ void Addmod_progression_systemScripts()
     AddBracket_60_2_A_Scripts();
     AddBracket_60_3_A_Scripts();
     AddBracket_60_3_B_Scripts();
+    AddBracket_60_3_C_Scripts();
     AddBracket_61_64_Scripts();
     AddBracket_65_69_Scripts();
     AddBracket_70_4_Scripts();
