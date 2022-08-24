6a59a930e96b30d333aab65c946e79a41f37e434
temperrr <speudoniem@outlook.com>
2022-08-24 14:58:27 +0200

    fix bracket loading

---
+++ src/Bracket_60_3_3/sql/Bracket_60_3_3_loader.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
+ */
+
+#include "ProgressionSystem.h"
+
+void AddBracket_60_3_C_Scripts()
+{
+    if (!(sConfigMgr->GetOption<int>("ProgressionSystem.Brackets", 0) & PROGRESSION_BRACKET_60_TIER_3_C_AHN_QIRAJ))
+        return;
+}
