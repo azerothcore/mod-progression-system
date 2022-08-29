925f9d11f36491acf5162f4090db1dea78aca2d4
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-08-29 19:58:01 -0300

    fix: remove Bolvar visibility condition

--- src/Bracket_60_3_2/sql/world/progression_60_3_2_onyxia_requirement.sql
+++ src/Bracket_60_3_2/sql/world/progression_60_3_2_onyxia_requirement.sql
@@ -1,1 +1,4 @@
 DELETE FROM `dungeon_access_requirements` WHERE `dungeon_access_id` = 15;
+
+-- Bolvar visibility
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 30 AND `SourceEntry` = 1748;
