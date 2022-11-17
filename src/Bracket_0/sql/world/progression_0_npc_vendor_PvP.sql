2761643e41b4bcaa1cd8b0929cdb24507fd488d4
Gultask <sagemochi@hotmail.com>
2022-11-17 16:42:01 -0300

    temp

--- src/Bracket_0/sql/world/progression_0_npc_vendor_PvP.sql
+++ src/Bracket_0/sql/world/progression_0_npc_vendor_PvP.sql
@@ -26,13 +26,6 @@ DELETE FROM `npc_vendor` WHERE `entry`=15126 AND `item` IN (20068, 20072, 20150,
 DELETE FROM `npc_vendor` WHERE `entry`=15127 AND `item` IN (20041, 20042, 20043, 20044, 20045, 20046, 20047, 20048, 20049, 20050, 20051, 20052, 20053, 20054, 20055, 20056, 20057, 20058, 20059, 20060, 20061, 20069, 20070, 20071, 20073);
 
 /*
-  Lhara in Darkmoon Faire, entry: 14846
-  Removes TBC and WotLK items
-  items: (21887, 22572, 22573, 22574, 22575, 22576, 22577, 22578, 22787, 22789, 22790, 22791, 22792, 22793, 22794, 23436, 23437, 23438, 23439, 23440, 23441, 23793, 25707, 25708, 33568, 36901, 36903, 36904, 36905, 36906, 36907, 36908, 37700, 37701, 37702, 37703, 37704, 37705, 37921, 38425, 44128, 46812)
-*/
-DELETE FROM `npc_vendor` WHERE (`entry` = 14846) AND (`item` IN (21887, 22572, 22573, 22574, 22575, 22576, 22577, 22578, 22787, 22789, 22790, 22791, 22792, 22793, 22794, 23436, 23437, 23438, 23439, 23440, 23441, 23793, 25707, 25708, 33568, 36901, 36903, 36904, 36905, 36906, 36907, 36908, 37700, 37701, 37702, 37703, 37704, 37705, 37921, 38425, 44128, 46812));
-
-/*
   Alterac Valley General Goods vendors, entry: 5134 and 3625
   Removes TBC and WotLK Ammunition
   items: (41584, 41586, 28056, 28061)
@@ -52,3 +45,45 @@ DELETE `npc_vendor`FROM `npc_vendor` WHERE `entry` IN (13218, 13216) AND `item` 
   items: (28060, 28053)
 */
 DELETE `npc_vendor` FROM `npc_vendor` WHERE `item` IN (28060, 28053);
+
+/*
+  Stormwind PVP Vendor Samuel Hawke, entry 15127
+  Removes 60 epic items
+  items(20055, 20056, 20057, 20058, 20059, 20060, 20061, 20069, 20070, 20073)
+*/
+DELETE FROM `npc_vendor` WHERE `entry` = 15127 AND `item` IN (20055, 20056, 20057, 20058, 20059, 20060, 20061, 20069, 20070, 20073);
+
+/*
+  Stormwind PVP Vendor Illiyana Moonblaze, entry 14753
+  Removes 60 epic items
+  items(19578, 19582, 19587, 19595, 22672, 22748, 22749, 22750, 22752, 22753, 30497)
+*/
+DELETE FROM `npc_vendor` WHERE `entry` = 14753 AND `item` IN (19578, 19582, 19587, 19595, 22672, 22748, 22749, 22750, 22752, 22753, 30497);
+
+/*
+  Stormwind PVP Vendor Thanthaldis Snowgleam, entry 13217
+  Removes 60 epic items
+  items(19308, 19309, 19310, 19311, 19312, 19315, 19321, 19323, 19324, 19325, 21563)
+*/
+DELETE FROM `npc_vendor` WHERE `entry` = 13217 AND `item` IN (19308, 19309, 19310, 19311, 19312, 19315, 19321, 19323, 19324, 19325, 21563);
+
+/*
+  Orgrimmar PVP Vendor Rutherford Twing, entry 15126
+  Removes 60 epic items
+  items(20068, 20158, 20175, 20176, 20184, 20194, 20203, 20212, 20214, 20220)
+*/
+DELETE FROM `npc_vendor` WHERE `entry` = 15126 AND `item` IN (20068, 20158, 20175, 20176, 20184, 20194, 20203, 20212, 20214, 20220);
+
+/*
+  Orgrimmar PVP Vendor Jekyll Flandring, entry 13219
+  Removes 60 epic items
+  items(19308, 19309, 19310, 19311, 19312, 19315, 19321, 19323, 19324, 19325, 21563)
+*/
+DELETE FROM `npc_vendor` WHERE `entry` = 13219 AND `item` IN (19308, 19309, 19310, 19311, 19312, 19315, 19321, 19323, 19324, 19325, 21563);
+
+/*
+  Orgrimmar PVP Vendor Kelm Hargunth, entry 14754
+  Removes 60 epic items
+  items(19578, 19582, 19587, 19595)
+*/
+DELETE FROM `npc_vendor` WHERE `entry` = 14754 AND `item` IN (19578, 19582, 19587, 19595);
