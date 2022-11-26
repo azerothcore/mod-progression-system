f281446d9e909d64981d4246245bf23cb0c6dd9e
Samsequel <speudoniem@outlook.com>
2022-11-26 16:34:12 +0100

    fix

--- src/ProgressionSystem.h
+++ src/ProgressionSystem.h
@@ -8,7 +8,7 @@
 #include "Config.h"
 #include "ScriptMgr.h"
 
-#define PROGRESSION_BRACKET_MAX 30
+#define PROGRESSION_BRACKET_MAX 33
 std::array<std::string, PROGRESSION_BRACKET_MAX> const ProgressionBracketsNames =
 {
     "0",
