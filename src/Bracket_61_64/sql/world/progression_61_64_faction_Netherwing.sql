93f23c7af15081e7f5905da3389ae4422ea8321d
Gultask <sagemochi@hotmail.com>
2022-11-26 22:19:23 -0300

    fix

--- src/Bracket_61_64/sql/world/progression_61_64_faction_Netherwing.sql
+++ src/Bracket_61_64/sql/world/progression_61_64_faction_Netherwing.sql
@@ -1,7 +1,7 @@
 -- Disable quest that unlocks Netherwing (previous chain kept open) and following quests
 DELETE FROM `disables` WHERE `sourceType`=1 AND `entry` IN (11012,11013,11015,11016,11017,11018,11020,11035,11041,11049,11050,11053,11054,11055,11064,11067,11068,11069,11070,11071,11075,11076,11077,11081,11082,11083,11084,11086,11089,11090,11092,11094,11097,11099,11101,11107,11108);
 INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
-(1, 11012, 'Blood Oath of the Netherwing');
+(1, 11012, 'Blood Oath of the Netherwing'),
 (1, 11013, 'In Service of the Illidari'),
 (1, 11015, 'Netherwing Crystals'),
 (1, 11016, 'Nethermine Flayer Hide'),
