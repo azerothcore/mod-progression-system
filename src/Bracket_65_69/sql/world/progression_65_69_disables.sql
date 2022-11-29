3e71370fe2f0da81b4064cf091e57234943b762e
Gultask <sagemochi@hotmail.com>
2022-11-28 22:46:08 -0300

    move OtDP to 70

--- src/Bracket_65_69/sql/world/progression_65_69_disables.sql
+++ src/Bracket_65_69/sql/world/progression_65_69_disables.sql
@@ -1,2 +1,2 @@
--- 65-69 level range - The Black Morass, Sethekk Halls, Auchenai Crypts, The Escape from Durnholde
-UPDATE `disables` SET `flags`=`flags`&~1 WHERE `entry` IN (269, 556, 558, 560);
+-- 65-69 level range - Sethekk Halls, Auchenai Crypts, The Escape from Durnholde
+UPDATE `disables` SET `flags`=`flags`&~1 WHERE `entry` IN (556, 558, 560);
--- src/Bracket_70_1_1/sql/world/progression_70_1_1_disables.sql
+++ src/Bracket_70_1_1/sql/world/progression_70_1_1_disables.sql
@@ -1,2 +1,2 @@
--- 70 level range - Level 70 Normals - The Shattered Halls, The Steamvault, The Arcatraz, The Botanica, The Mechanar, Shadow Labyrinth
-UPDATE `disables` SET `flags`=`flags`&~1 WHERE `entry` IN (540, 545, 552, 553, 554, 555);
+-- 70 level range - Level 70 Normals - Opening the Dark Portal, The Shattered Halls, The Steamvault, The Arcatraz, The Botanica, The Mechanar, Shadow Labyrinth
+UPDATE `disables` SET `flags`=`flags`&~1 WHERE `entry` IN (269, 540, 545, 552, 553, 554, 555);
