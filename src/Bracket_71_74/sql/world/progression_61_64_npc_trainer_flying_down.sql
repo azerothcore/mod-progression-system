328b9e75aab2f5585c9f12186dd008ec1027593e
Gultask <sagemochi@hotmail.com>
2022-11-30 08:30:27 -0300

    fix

--- src/Bracket_71_74/sql/world/progression_61_64_npc_trainer_flying_down.sql
+++ src/Bracket_71_74/sql/world/progression_61_64_npc_trainer_flying_down.sql
@@ -1,1 +1,1 @@
-UPDATE `npc_trainer` SET `MoneyCost`=2500000, `ReqLevel`=60 WHERE  `ID`=202011 AND `SpellID`=34090;
+UPDATE `npc_trainer` SET `MoneyCost`=2500000, `ReqLevel`=60 WHERE `ID`=202011 AND `SpellID`=34090;
