b420b5a368e82c037fc67da07100554fe2f8da48
Gultask <sagemochi@hotmail.com>
2022-12-04 17:38:48 -0300

    epic_flight

--- src/Bracket_61_64/sql/world/progression_61_64_npc_trainer_flying.sql
+++ src/Bracket_61_64/sql/world/progression_61_64_npc_trainer_flying.sql
@@ -1,1 +1,5 @@
+-- Update cost for normal flying
 UPDATE `npc_trainer` SET `MoneyCost`=8000000, `ReqLevel`=70 WHERE `ID`=202011 AND `SpellID`=34090;
+
+-- Remove Epic Flight Form from trainers
+DELETE FROM `npc_trainer` WHERE `ID`=200006 AND `SpellID`=40120;
--- src/Bracket_71_74/sql/world/progression_61_64_npc_trainer_flying_down.sql
+++ src/Bracket_71_74/sql/world/progression_61_64_npc_trainer_flying_down.sql
@@ -1,1 +1,5 @@
+-- Update cost for normal flying back to original value
 UPDATE `npc_trainer` SET `MoneyCost`=2500000, `ReqLevel`=60 WHERE `ID`=202011 AND `SpellID`=34090;
+
+-- Restore Epic Flight Form from trainers
+INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES (200006, 40120, 200000, 762, 300, 71, 0);
