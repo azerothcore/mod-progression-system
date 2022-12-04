1babeee3b8421b54a45e2980a35e55b789497658
Gultask <sagemochi@hotmail.com>
2022-12-04 20:12:27 -0300

    fix

--- src/Bracket_61_64/sql/world/progression_61_64_dungeon_access_requirements_attunements.sql
+++ src/Bracket_61_64/sql/world/progression_61_64_dungeon_access_requirements_attunements.sql
@@ -7,7 +7,7 @@ INSERT INTO `dungeon_access_requirements` (`dungeon_access_id`, `requirement_typ
 (49, 2, 31084, (NULL), (NULL), 'Progression: Tempest Keep: The Arcatraz (Heroic)'),
 (54, 2, 27991, (NULL), (NULL), 'Progression: Auchindoun: Shadow Labyrinth (Normal)'),
 (55, 2, 27991, (NULL), (NULL), 'Progression: Auchindoun: Shadow Labyrinth (Heroic)'),
-(29, 2, 27991, (NULL), (NULL), 'Progression: Phase 1: Karazhan'),
+(29, 2, 24490, (NULL), (NULL), 'Progression: Phase 1: Karazhan'),
 (46, 1, 10901, (NULL), 'You must complete the quest "The Cudgel of Kar\'desh" starting at "Skar\'this the Heretic" before entering the Serpentshrine Cavern.', 'Progression: Phase 2: Serpentshrine Cavern'),
 (47, 2, 31704, (NULL), (NULL), 'Progression: Phase 2: The Eye'),
 (32, 1, 10445, (NULL), 'You must complete the quest "The Vials of Eternity" before entering the Hyjal Summit.', 'Progression: Phase 3: Battle for Mount Hyjal'),
