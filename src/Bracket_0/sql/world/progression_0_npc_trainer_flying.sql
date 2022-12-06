258c902e41909f5b93f7eac45cf3a54074231c30
Gultask <sagemochi@hotmail.com>
2022-12-06 19:56:01 -0300

    flight form to 68

--- src/Bracket_0/sql/world/progression_0_npc_trainer_flying.sql
+++ src/Bracket_0/sql/world/progression_0_npc_trainer_flying.sql
@@ -3,3 +3,6 @@ UPDATE `npc_trainer` SET `MoneyCost`=8000000, `ReqLevel`=70 WHERE `ID`=202011 AN
 
 -- Remove Epic Flight Form from trainers
 DELETE FROM `npc_trainer` WHERE `ID`=200006 AND `SpellID`=40120;
+
+-- Lock Flight Form to 68
+UPDATE `npc_trainer` SET `ReqLevel`=68 WHERE `ID`=200006 AND `SpellID`=33950;
