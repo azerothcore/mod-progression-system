e05d9ab87bee76d0b9308b85245363e2d402a619
Gultask <sagemochi@hotmail.com>
2022-12-06 22:30:15 -0300

    fix quest id

--- src/Bracket_61_64/sql/world/progression_61_64_quest_template_attunement.sql
+++ src/Bracket_61_64/sql/world/progression_61_64_quest_template_attunement.sql
@@ -1,6 +1,6 @@
 -- Adds The Tempest Key, and the title Champion of the Naaru to the rewards of Trial of the Naaru: Magtheridon
 UPDATE `quest_template` SET `RewardItem2` = 31704, `RewardAmount2` = 1, `RewardTitle` = 53 WHERE (`ID` = 13430);
 -- Adds The Mark of Vashj spell to The Cudgel of Kar'desh (cosmetic)
-UPDATE `quest_template` SET `RewardSpell` = 39145 WHERE (`ID` = 10901);
+UPDATE `quest_template` SET `RewardSpell` = 39145 WHERE (`ID` = 13431);
 -- Add Hand of A'dal Title reward for completing BT attunement
 UPDATE `quest_template` SET `RewardTitle` = 64 WHERE (`ID` = 10985);
