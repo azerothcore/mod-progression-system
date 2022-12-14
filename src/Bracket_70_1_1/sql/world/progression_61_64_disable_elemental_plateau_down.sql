a251548cd741eb66d6bea1a7ba58dd18d96fbbb9
Gultask <sagemochi@hotmail.com>
2022-12-14 04:02:06 -0300

    1

---
+++ src/Bracket_70_1_1/sql/world/progression_61_64_disable_elemental_plateau_down.sql
@@ -0,0 +1,16 @@
+UPDATE `creature` SET `phasemask` = 1 WHERE `id1` IN (
+22309, -- Crashing Wave-Spirit
+22310, -- Stormwind Wind-Ripper
+22311, -- Raging Fire-Soul
+22313 -- Rumbling Earth-Heart
+);
+
+-- Pure Water
+UPDATE `gameobject` SET `phasemask` = 1 WHERE `guid` IN (
+104324,
+104325,
+104326,
+104327,
+104328,
+104329
+) AND `id`=182951;
