64d4653058273031e1ac7ac6d05e19c5df114941
Gultask <sagemochi@hotmail.com>
2022-12-17 21:13:11 -0300

    1

---
+++ src/Bracket_61_64/sql/world/progression_0_areatrigger_teleport_dark_portal_down.sql
@@ -0,0 +1,2 @@
+-- Change areatrigger_teleport for Dark Portal (No level restriction on WotLK Classic)
+UPDATE `areatrigger_teleport` SET `target_map`=530, `target_position_x`=-248.149, `target_position_y`=921.875, `target_position_z`=84.3885, `target_orientation`=1.58415 WHERE `ID`=4354;
