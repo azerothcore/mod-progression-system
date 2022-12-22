3c5b1c471b674aa7fa691c67068f65848f4ac9bb
Gultask <100873791+Gultask@users.noreply.github.com>
2022-12-22 12:23:22 -0300

    wheredidthathappen (#281)

--- src/Bracket_61_64/sql/world/progression_61_64_item_template_revered_heroic_keys.sql
+++ src/Bracket_61_64/sql/world/progression_61_64_item_template_revered_heroic_keys.sql
@@ -1,5 +1,5 @@
 -- Set Dungeon Heroic Keys to require Revered with their respective factions
-UPDATE `item_template` SET `RequiredReputationRank` = 6 WHERE (`entry` IN (
+UPDATE `item_template` SET `RequiredReputationRank` = 6 WHERE `entry` IN (
 30622, -- Hellfire Citadel (Alliance)
 30623, -- Coilfang Resevoir
 30633, -- Auchindoun
