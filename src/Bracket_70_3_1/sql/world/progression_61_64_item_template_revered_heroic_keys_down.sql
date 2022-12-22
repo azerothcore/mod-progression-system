469fadf3ed0ddcc6eeec46d9d856a8cb118f6a3f
Gultask <sagemochi@hotmail.com>
2022-12-22 12:21:50 -0300

    wheredidthathappen

--- src/Bracket_70_3_1/sql/world/progression_61_64_item_template_revered_heroic_keys_down.sql
+++ src/Bracket_70_3_1/sql/world/progression_61_64_item_template_revered_heroic_keys_down.sql
@@ -1,5 +1,5 @@
 -- Restore Dungeon Heroic Keys to require Honored with their respective factions
-UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE (`entry` IN (
+UPDATE `item_template` SET `RequiredReputationRank` = 5 WHERE `entry` IN (
 30622, -- Hellfire Citadel (Alliance)
 30623, -- Coilfang Resevoir
 30633, -- Auchindoun
