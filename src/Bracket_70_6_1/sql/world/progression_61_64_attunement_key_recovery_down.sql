0881bece9f1b44a4b172892c2755788d83f2bb0f
Gultask <sagemochi@hotmail.com>
2023-01-30 20:31:46 -0300

    Fix Rohok and Dumphry not being trainers

--- src/Bracket_70_6_1/sql/world/progression_61_64_attunement_key_recovery_down.sql
+++ src/Bracket_70_6_1/sql/world/progression_61_64_attunement_key_recovery_down.sql
@@ -12,19 +12,3 @@ DELETE FROM `gossip_menu_option` WHERE (`MenuID` = 7966) AND (`OptionID` IN (0, 
 DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 15) AND (`SourceGroup` = 7966);
 
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 18481) AND (`source_type` = 0) AND (`id` IN (5, 6, 7, 8));
-
--- Rohok (Shattered Halls - Horde)
-DELETE FROM `gossip_menu_option` WHERE (`MenuID` = 8760) AND (`OptionID` IN (0));
-
-DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 15) AND (`SourceGroup` = 8760);
-
-UPDATE `creature_template` SET `AIName` = '' WHERE `entry` = 16583;
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 16583) AND (`source_type` = 0) AND (`id` IN (0, 1));
-
--- Dumphry (Shattered Halls - Alliance)
-DELETE FROM `gossip_menu_option` WHERE (`MenuID` = 8254) AND (`OptionID` IN (0));
-
-DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 15) AND (`SourceGroup` = 8254);
-
-UPDATE `creature_template` SET `AIName` = '' WHERE `entry` = 21209;
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 21209) AND (`source_type` = 0) AND (`id` IN (0, 1));
