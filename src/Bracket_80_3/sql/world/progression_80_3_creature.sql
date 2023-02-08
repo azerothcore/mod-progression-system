03ca867e4581035b5b8a40992800d175bbb14f25
mpfans <mpfans@vip.qq.com>
2023-02-08 20:27:02 +0800

    Update progression_80_3_creature.sql (#300)

--- src/Bracket_80_3/sql/world/progression_80_3_creature.sql
+++ src/Bracket_80_3/sql/world/progression_80_3_creature.sql
@@ -1,3 +1,4 @@
 -- 80 level range - Tier 9 & Relentless Gladiator
 -- restore Koralon at Vaulth of Archavon
 UPDATE `creature` SET `phaseMask` = 1 WHERE `id1` = 35013;
+UPDATE `creature` SET `phaseMask` = 1 WHERE `id1` = 35360;
