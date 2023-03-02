8f36eeaf103ea9c2ea706a8d4add6d068a1c5e3a
Gultask <sagemochi@hotmail.com>
2023-03-02 13:26:55 -0300

    2

--- src/Bracket_61_64/sql/world/progression_50_59_1_lfg_dungeon_rewards_down_1.sql
+++ src/Bracket_61_64/sql/world/progression_50_59_1_lfg_dungeon_rewards_down_1.sql
@@ -1,5 +1,2 @@
 -- restore the original value (TBC) for the satchel for level 
 UPDATE `lfg_dungeon_rewards` SET `firstQuestId` = 24887, `otherQuestId`=24895 WHERE `dungeonId`=259 AND `maxLevel`=64;
-
--- Do the same for WotLK
-UPDATE `lfg_dungeon_rewards` SET `firstQuestId` = 24790, `otherQuestId`=24791 WHERE `dungeonId`=261 AND `maxLevel`=80;
