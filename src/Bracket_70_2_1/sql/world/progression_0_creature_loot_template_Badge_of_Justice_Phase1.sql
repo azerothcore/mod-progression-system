d0ad7a1a67c7801a1f476899cd6071b6e97d9da3
Gultask <100873791+Gultask@users.noreply.github.com>
2023-07-11 23:14:19 -0300

    (SQL) Remove Badge of Justice from Phase 1 Raids (#341)
    Co-authored-by: Samsequel <20357406+Samsequel@users.noreply.github.com>

---
+++ src/Bracket_70_2_1/sql/world/progression_0_creature_loot_template_Badge_of_Justice_Phase1.sql
@@ -0,0 +1,2 @@
+-- Remove Badge of Justice from Phase 1 Raids
+DELETE FROM `creature_loot_template` WHERE `Entry` IN  (17225, 15690, 15688, 15689, 16524, 15691, 17533, 18168, 17521, 16457, 15687, 16152, 19044, 18831, 17257) AND (`Item` IN (29434));
