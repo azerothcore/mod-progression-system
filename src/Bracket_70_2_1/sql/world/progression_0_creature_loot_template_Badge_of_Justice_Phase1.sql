d0a048abe7741ade12c06cdb5b9471da90327e78
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-07-30 17:38:01 +0200

    Update progression_0_creature_loot_template_Badge_of_Justice_Phase1.sql

--- src/Bracket_70_2_1/sql/world/progression_0_creature_loot_template_Badge_of_Justice_Phase1.sql
+++ src/Bracket_70_2_1/sql/world/progression_0_creature_loot_template_Badge_of_Justice_Phase1.sql
@@ -1,13 +1,12 @@
 -- Remove Badge of Justice from Phase 1 Raids
-DELETE FROM `creature_loot_template` WHERE `Entry` IN  (17225, 15690, 15688, 15689, 16524, 15691, 17533, 18168, 17521, 16457, 15687, 16152) AND (`Item` IN (29434));
-
-
+DELETE FROM `creature_loot_template` WHERE `Entry` IN  (17225, 15690, 15688, 15689, 16524, 15691, 17533, 18168, 17521, 16457, 15687, 16152, 19044, 18831, 17257) AND (`Item` IN (29434));
+-- gruul loot nerfed
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 19044);
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (19044, 31750, 0, 100, 1, 1, 0, 1, 1, 'Gruul the Dragonkiller - Earthen Signet'),
 (19044, 34051, 34051, 100, 0, 1, 2, 1, 2, 'Gruul the Dragonkiller - (ReferenceTable)'),
 (19044, 190039, 34051, 100, 0, 1, 1, 1, 1, 'Gruul the Dragonkiller - (ReferenceTable)');
-
+-- maulgar loot nerfed
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 18831);
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (18831, 28795, 0, 0, 0, 1, 1, 1, 1, 'High King Maulgar - Bladespire Warbands'),
@@ -17,7 +16,7 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 (18831, 28800, 0, 0, 0, 1, 1, 1, 1, 'High King Maulgar - Hammer of the Naaru'),
 (18831, 28801, 0, 0, 0, 1, 1, 1, 1, 'High King Maulgar - Maulgar\'s Warhelm'),
 (18831, 34050, 34050, 100, 0, 1, 1, 1, 1, 'High King Maulgar - (ReferenceTable)');
-
+-- matheridon loot nerfed
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 17257);
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (17257, 32385, 0, 100, 0, 1, 0, 1, 1, 'Magtheridon - Magtheridon\'s Head'),
