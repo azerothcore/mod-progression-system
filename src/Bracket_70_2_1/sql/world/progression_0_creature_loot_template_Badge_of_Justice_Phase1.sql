44b5f4f1744bc16009c916b8a26023808c3dd7a1
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-07-30 17:36:53 +0200

    Update progression_0_creature_loot_template_Badge_of_Justice_Phase1.sql

--- src/Bracket_70_2_1/sql/world/progression_0_creature_loot_template_Badge_of_Justice_Phase1.sql
+++ src/Bracket_70_2_1/sql/world/progression_0_creature_loot_template_Badge_of_Justice_Phase1.sql
@@ -4,7 +4,6 @@ DELETE FROM `creature_loot_template` WHERE `Entry` IN  (17225, 15690, 15688, 156
 
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 19044);
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(19044, 29434, 0, 100, 0, 1, 0, 3, 3, 'Gruul the Dragonkiller - Badge of Justice'),
 (19044, 31750, 0, 100, 1, 1, 0, 1, 1, 'Gruul the Dragonkiller - Earthen Signet'),
 (19044, 34051, 34051, 100, 0, 1, 2, 1, 2, 'Gruul the Dragonkiller - (ReferenceTable)'),
 (19044, 190039, 34051, 100, 0, 1, 1, 1, 1, 'Gruul the Dragonkiller - (ReferenceTable)');
@@ -17,12 +16,10 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 (18831, 28799, 0, 0, 0, 1, 1, 1, 1, 'High King Maulgar - Belt of Divine Inspiration'),
 (18831, 28800, 0, 0, 0, 1, 1, 1, 1, 'High King Maulgar - Hammer of the Naaru'),
 (18831, 28801, 0, 0, 0, 1, 1, 1, 1, 'High King Maulgar - Maulgar\'s Warhelm'),
-(18831, 29434, 0, 100, 0, 1, 0, 2, 2, 'High King Maulgar - Badge of Justice'),
 (18831, 34050, 34050, 100, 0, 1, 1, 1, 1, 'High King Maulgar - (ReferenceTable)');
 
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 17257);
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(17257, 29434, 0, 100, 0, 1, 0, 3, 3, 'Magtheridon - Badge of Justice'),
 (17257, 32385, 0, 100, 0, 1, 0, 1, 1, 'Magtheridon - Magtheridon\'s Head'),
 (17257, 32386, 0, 100, 0, 1, 0, 1, 1, 'Magtheridon - Magtheridon\'s Head'),
 (17257, 34039, 34039, 100, 0, 1, 1, 1, 1, 'Magtheridon - (ReferenceTable)'),
