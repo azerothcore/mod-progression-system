3924e4537116ee63569970033fb6b6348a545495
Maria-sequel <20357406+Maria-sequel@users.noreply.github.com>
2023-08-04 21:16:35 +0200

    fix(DB/Loot): kara loot progression changes (#343)
    * fix(db/loot): kara loot progression changes
    
    * Create progression_71_74_creature_loot_template_kara_enchants_100.sql
    
    * philantrophist
    
    * restore
    
    ---------
    
    Co-authored-by: Samsequel <20357406+Samsequel@users.noreply.github.com>

--- src/Bracket_80_2/sql/world/progression_80_2_creature.sql
+++ src/Bracket_80_2/sql/world/progression_80_2_creature.sql
@@ -1,3 +1,5 @@
 -- 80 level range - Tier 8 & Furious Gladiator
 -- restore Emalon at Vaulth of Archavon
 UPDATE `creature` SET `phaseMask` = 1 WHERE `id1` = 33993;
+-- restore no gold from kara philantropist
+UPDATE `creature_template` SET `mingold` = 0, `maxgold` = 0 WHERE (`entry` = 16470);
