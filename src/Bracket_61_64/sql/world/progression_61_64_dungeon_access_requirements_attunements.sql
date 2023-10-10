8d8d132b12907ef7f98db1f71f6bd89107825b9a
Dan <83884799+elthehablo@users.noreply.github.com>
2023-10-10 20:30:38 +0200

    initial

--- src/Bracket_61_64/sql/world/progression_61_64_dungeon_access_requirements_attunements.sql
+++ src/Bracket_61_64/sql/world/progression_61_64_dungeon_access_requirements_attunements.sql
@@ -8,7 +8,7 @@ INSERT INTO `dungeon_access_requirements` (`dungeon_access_id`, `requirement_typ
 (54, 2, 27991, 1, (NULL), 'Progression: Auchindoun: Shadow Labyrinth (Normal)'),
 (55, 2, 27991, 1, (NULL), 'Progression: Auchindoun: Shadow Labyrinth (Heroic)'),
 (29, 2, 24490, 0, 'You must have the item The Master\'s Key to enter Karazhan.', 'Progression: Phase 1: Karazhan'),
-(46, 1, 10901, 0, 'You must complete the quest "The Cudgel of Kar\'desh" starting at "Skar\'this the Heretic" before entering the Serpentshrine Cavern.', 'Progression: Phase 2: Serpentshrine Cavern'),
+(46, 1, 13431, 0, 'You must complete the quest "The Cudgel of Kar\'desh" starting at "Skar\'this the Heretic" before entering the Serpentshrine Cavern.', 'Progression: Phase 2: Serpentshrine Cavern'),
 (47, 2, 31704, 0, 'You must have the item The Tempest Key to enter The Eye.', 'Progression: Phase 2: The Eye'),
 (32, 1, 13432, 0, 'You must complete the quest "The Vials of Eternity" before entering the Hyjal Summit.', 'Progression: Phase 3: Battle for Mount Hyjal'),
 (64, 1, 10985, 0, 'You must have the item Medallion of Karabor to enter the Black Temple.', 'Progression: Phase 3: The Black Temple');
