99f3df9c4bb6f867fd9a7c4ec1b087af46d95037
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-10-21 15:47:25 -0300

    fix(DB/Loot): Remove Badge of Justice from Karazhan chess

--- src/Bracket_80_2/sql/world/progression_80_2_creature.sql
+++ src/Bracket_80_2/sql/world/progression_80_2_creature.sql
@@ -3,3 +3,7 @@
 UPDATE `creature` SET `phaseMask` = 1 WHERE `id1` = 33993;
 -- restore no gold from kara philantropist
 UPDATE `creature_template` SET `mingold` = 0, `maxgold` = 0 WHERE (`entry` = 16470);
+-- Restore Badge of Justice to kara chess
+DELETE FROM `gameobject_loot_template` WHERE `Item` = 29434 AND `Entry` = 20712;
+INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(20712,29434,0,100,0,1,0,1,1,'Dust Covered Chest - Badge of Justice');
