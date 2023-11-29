6bc86adea609d26d86351d4d55b403e2dd8b9732
Andrew <47818697+Nyeriah@users.noreply.github.com>
2023-11-29 00:47:34 -0300

    fix:  move ogrilla to own bracket (#363)

---
+++ src/Bracket_70_2_3/Bracket_70_2_3_loader.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
+ */
+
+#include "ProgressionSystem.h"
+
+void AddSC_karazhan_70();
+
+void AddBracket_70_2_B_Scripts()
+{
+    if (!(sConfigMgr->GetOption<bool>("ProgressionSystem.Bracket_70_2_2", false)))
+        return;
+
+    AddSC_karazhan_70();
+}
