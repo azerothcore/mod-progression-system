97b2def3f018cabfbc4b11feff2d34053a583736
Andrew <47818697+Nyeriah@users.noreply.github.com>
2023-12-13 15:45:33 -0300

    fix: dont count the lurker in ssc progression (#370)

--- src/Bracket_70_3_1/scripts/SerpentshrineCavern/serpentshrine_cavern.cpp
+++ src/Bracket_70_3_1/scripts/SerpentshrineCavern/serpentshrine_cavern.cpp
@@ -11,6 +11,7 @@ enum SSCMisc
 {
     GO_LADY_VASHJ_BRIDGE_CONSOLE = 184568,
     MAP_SSC                      = 548,
+    DATA_LURKER                  = 1,
     DATA_VASHJ                   = 6
 };
 
@@ -55,6 +56,11 @@ public:
                         for (uint8 id = 0; id <= bossCount; ++id)
                         {
                             if (id == bossId && newState == DONE)
+                            {
+                                continue;
+                            }
+
+                            if (id == DATA_LURKER)
                             {
                                 continue;
                             }
