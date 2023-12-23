8dc03100fc21edeec63fc5e52fe6f18ab4bb9d9e
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2023-12-22 19:54:33 -0500

    Create progression_80_2_ArgentTournamentConstructionQuests.sql

---
+++ src/Bracket_80_2/sql/world/progression_80_2_ArgentTournamentConstructionQuests.sql
@@ -0,0 +1,9 @@
+INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES
+(33817, 13820), -- The Blastbolt Brothers
+(33435, 13681), -- A Chip Off the Ulduar Block
+(33434, 13627); -- Jack Me Some Lumber
+
+INSERT INTO `creature_questender` (`id`, `quest`) VALUES
+(33434, 13820), -- The Blastbolt Brothers
+(33435, 13681), -- A Chip Off the Ulduar Block
+(33434, 13627); -- Jack Me Some Lumber
