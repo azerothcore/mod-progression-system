ac748767cfdf4a961cde4c63b31aed67691897fc
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2024-01-09 22:18:20 -0300

    feat: PvP arena point dailies
    Co-Authored-By: 55Honey <71938210+55Honey@users.noreply.github.com>

---
+++ src/Bracket_71_74/sql/world/progression_70_1_1_pvp_dailies_down.sql
@@ -0,0 +1,8 @@
+UPDATE `quest_template` SET
+`MinLevel` = 71
+WHERE ID IN(
+11335, 11339, -- Arathi Basin
+11336, 11340, -- Alterac Valley
+11337, 11341, -- Eye of the Storm
+11338, 11342  -- Warsong Gulch
+);
