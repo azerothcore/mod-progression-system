4d87f528e9b3595b460c2d0c8b3d82f16f1e454a
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2024-02-07 13:04:26 -0500

    Add progression stages for Love is in the Air's Bouquets of Red Roses to drop from dungeon bosses. (#383)
    * Up.
    
    * Up II.
    
    * Down.

--- src/Bracket_80_1/sql/world/progression_1_19_love_in_air_down.sql
+++ src/Bracket_80_1/sql/world/progression_1_19_love_in_air_down.sql
@@ -10,6 +10,10 @@ INSERT INTO `item_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Quest
 (54537,49927,0,100,0,1,0,5,10,'Heart-Shaped Box - Love Token'),
 (54537,50250,0,0.1,0,1,0,1,1,'Heart-Shaped Box - Big Love Rocket');
 
+-- Bouquet of Red Roses
+DELETE FROM `creature_loot_template` WHERE (`Entry` IN (8929, 10811, 10901, 11488)) AND (`Item` = 22206);
+DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 1) AND (`SourceGroup` IN (8929, 10811, 10901, 11488)) AND (`SourceEntry` = 22206) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 12) AND (`ConditionTarget` = 0) AND (`ConditionValue1` = 8) AND (`ConditionValue2` = 0) AND (`ConditionValue3` = 0);
+
 UPDATE `creature_template` SET `minlevel` = 82, `maxlevel` = 82 WHERE `entry` IN (36565, 36296, 36272);
 UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` = 36568; -- crazed apothecary
 UPDATE `creature_template` SET `lootid` = 36296 WHERE `entry` = 36296;
