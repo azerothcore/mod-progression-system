ae4b32247d92688ff0645f06fe86e907d852f2e3
Dan <83884799+elthehablo@users.noreply.github.com>
2024-03-12 23:37:17 +0100

    fix(DB/TheEye): remove boj from drops (#387)
    * init
    
    * down
    
    * Update progression_0_creature_loot_template_Badge_of_Justice_Phase1_down.sql
    
    * Update progression_0_creature_loot_template_Badge_of_Justice_Phase1_down.sql

--- src/Bracket_70_2_1/sql/world/progression_0_creature_loot_template_Badge_of_Justice_Phase1.sql
+++ src/Bracket_70_2_1/sql/world/progression_0_creature_loot_template_Badge_of_Justice_Phase1.sql
@@ -1,5 +1,5 @@
 -- Remove Badge of Justice from Phase 1 Raids
-DELETE FROM `creature_loot_template` WHERE `Entry` IN  (17225, 15690, 15688, 15689, 16524, 15691, 17533, 18168, 17521, 16457, 15687, 16152, 21216, 21217, 21215, 21214, 21213, 21212) AND (`Item` IN (29434));
+DELETE FROM `creature_loot_template` WHERE `Entry` IN  (17225, 15690, 15688, 15689, 16524, 15691, 17533, 18168, 17521, 16457, 15687, 16152, 21216, 21217, 21215, 21214, 21213, 21212, 19514, 19622, 19516, 18805) AND (`Item` IN (29434));
 -- gruul loot nerfed
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 19044);
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
--- src/Bracket_70_5/sql/world/progression_0_creature_loot_template_Badge_of_Justice_Phase1_down.sql
+++ src/Bracket_70_5/sql/world/progression_0_creature_loot_template_Badge_of_Justice_Phase1_down.sql
@@ -1,5 +1,5 @@
 -- Restore Badge of Justice from Phase 1 Raids
-DELETE FROM `creature_loot_template` WHERE `Entry` IN  (17225, 15690, 15688, 15689, 16524, 15691, 17533, 18168, 17521, 16457, 15687, 16152, 19044, 18831, 17257, 21216, 21217, 21215, 21214, 21213, 21212) AND (`Item` IN (29434));
+DELETE FROM `creature_loot_template` WHERE `Entry` IN  (17225, 15690, 15688, 15689, 16524, 15691, 17533, 18168, 17521, 16457, 15687, 16152, 19044, 18831, 17257, 21216, 21217, 21215, 21214, 21213, 21212, 19514, 19622, 19516, 18805) AND (`Item` IN (29434));
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (17257, 29434, 0, 100, 0, 1, 0, 3, 3, 'Magtheridon - Badge of Justice'),
 (18831, 29434, 0, 100, 0, 1, 0, 2, 2, 'High King Maulgar - Badge of Justice'),
@@ -21,4 +21,8 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 (21215, 29434, 0, 100, 0, 1, 0, 2, 2, 'Leotheras - Badge of Justice'),
 (21214, 29434, 0, 100, 0, 1, 0, 2, 2, 'Fathom-Lord Karathress - Badge of Justice'),
 (21213, 29434, 0, 100, 0, 1, 0, 2, 2, 'Morogrim - Badge of Justice'),
-(21212, 29434, 0, 100, 0, 1, 0, 2, 2, 'Lady Vashj - Badge of Justice');
+(21212, 29434, 0, 100, 0, 1, 0, 2, 2, 'Lady Vashj - Badge of Justice'),
+(19514, 29434, 0, 100, 0, 1, 0, 2, 2, 'Al\'ar - Badge of Justice'),
+(19622, 29434, 0, 100, 0, 1, 0, 2, 2, 'Kael\'thas - Badge of Justice'),
+(19516, 29434, 0, 100, 0, 1, 0, 2, 2, 'Void Reaver - Badge of Justice'),
+(18805, 29434, 0, 100, 0, 1, 0, 2, 2, 'Solarian - Badge of Justice');
