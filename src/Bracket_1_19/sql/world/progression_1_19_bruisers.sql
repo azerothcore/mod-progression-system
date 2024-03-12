e2de9d8b84ef86e69184f498571db94ac23cf56e
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2024-03-12 19:08:43 -0400

    Add progression stages for Booty Bay Bruiser levels. (#382)
    * Up.
    
    * Create progression_61_64_bruisers_vanilla_down.sql
    
    * Correct file name.
    
    * Create progression_1_19_bruisers_tbc_down.sql

---
+++ src/Bracket_1_19/sql/world/progression_1_19_bruisers.sql
@@ -0,0 +1,1 @@
+UPDATE `creature_template` SET `minlevel` = 57, `maxlevel` = 57 WHERE `entry` = 4624; -- Booty Bay Bruiser
---
+++ src/Bracket_61_64/sql/world/progression_1_19_bruisers_vanilla_down.sql
@@ -0,0 +1,1 @@
+UPDATE `creature_template` SET `minlevel` = 67, `maxlevel` = 67 WHERE `entry` = 4624; -- Booty Bay Bruiser
