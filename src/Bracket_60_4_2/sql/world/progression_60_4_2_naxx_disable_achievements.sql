a60492defcb43ce3953ae0464267eeb0b95b0b22
Jelle Meeus <sogladev@gmail.com>
2024-04-11 22:29:39 +0200

    style achievements disable

--- src/Bracket_60_4_2/sql/world/progression_60_4_2_naxx_disable_achievements.sql
+++ src/Bracket_60_4_2/sql/world/progression_60_4_2_naxx_disable_achievements.sql
@@ -1,6 +1,6 @@
 -- Disable 25man achievements
 -- https://wow.tools/dbc/?dbc=achievement_criteria&build=3.3.5.12340
-DELETE FROM `disables` WHERE `entry` IN 
+DELETE FROM `disables` WHERE `entry` IN
 (13233, 13234, 13235, 13236, 382, 3834, 3835, 3836, 3837, 3838, 3839, 3840,
 3841, 3842, 3843, 3844, 3845, 3847, 389, 390, 391, 392, 393, 393, 394, 5227,
 7127, 7129, 7159, 7160, 7161, 7162, 7163, 7164, 7165, 7166, 7167, 7168, 7169,
