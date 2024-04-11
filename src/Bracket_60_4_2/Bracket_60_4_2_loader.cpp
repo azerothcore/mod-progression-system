3d7b6896f31b8d74428754a8fe26606450fd4570
Jelle Meeus <sogladev@gmail.com>
2024-04-11 23:22:07 +0200

    add custom scripts to loader

--- src/Bracket_60_4_2/Bracket_60_4_2_loader.cpp
+++ src/Bracket_60_4_2/Bracket_60_4_2_loader.cpp
@@ -6,6 +6,22 @@
 
 void AddSC_naxx_entry_flag_60_4_B();
 void AddSC_omarion_60_4_B();
+void AddSC_instance_naxxramas_combined();
+void AddSC_boss_anubrekhan_40();
+void AddSC_boss_faerlina_40();
+void AddSC_boss_four_horsemen_40();
+void AddSC_boss_gluth_40();
+void AddSC_boss_gothik_40();
+void AddSC_boss_grobbulus_40();
+void AddSC_boss_heigan_40();
+void AddSC_boss_kelthuzad_40();
+void AddSC_boss_loatheb_40();
+void AddSC_boss_maexxna_40();
+void AddSC_boss_noth_40();
+void AddSC_boss_patchwerk_40();
+void AddSC_boss_razuvious_40();
+void AddSC_boss_sapphiron_40();
+void AddSC_boss_thaddius_40();
 
 void AddBracket_60_4_B_Scripts()
 {
@@ -13,4 +29,21 @@ void AddBracket_60_4_B_Scripts()
         return;
     AddSC_naxx_entry_flag_60_4_B();
     AddSC_omarion_60_4_B();
+    // Custom naxx40 scripts
+    void AddSC_instance_naxxramas_combined();
+    void AddSC_boss_anubrekhan_40();
+    void AddSC_boss_faerlina_40();
+    void AddSC_boss_four_horsemen_40();
+    void AddSC_boss_gluth_40();
+    void AddSC_boss_gothik_40();
+    void AddSC_boss_grobbulus_40();
+    void AddSC_boss_heigan_40();
+    void AddSC_boss_kelthuzad_40();
+    void AddSC_boss_loatheb_40();
+    void AddSC_boss_maexxna_40();
+    void AddSC_boss_noth_40();
+    void AddSC_boss_patchwerk_40();
+    void AddSC_boss_razuvious_40();
+    void AddSC_boss_sapphiron_40();
+    void AddSC_boss_thaddius_40();
 }
