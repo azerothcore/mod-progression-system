91f996aff727804037e09c1fc52a545a2c5b49f8
heyitsbench <benjymansy123@gmail.com>
2024-06-24 08:09:47 -0400

    Init.

--- src/Bracket_1_19/sql/world/progression_1_19_world_event_Ahune.sql
+++ src/Bracket_1_19/sql/world/progression_1_19_world_event_Ahune.sql
@@ -30,3 +30,9 @@ UPDATE `item_template` SET `RequiredLevel` = 65 WHERE (`entry` = 35723);
 
 UPDATE `creature_template` SET `MaxLevel` = 70, `MinLevel` = 70 WHERE `entry` IN (25755, 25756, 25757, 26340, 26341, 26342, 26339, 25865, 40446);
 UPDATE `creature_template` SET `MaxLevel` = 72, `MinLevel` = 72 WHERE `entry` IN (25740, 26338, 26865, 26339);
+
+-- Summon Loot Bunny SAI
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 25746 AND `source_type` = 0 AND `id` = 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(25746,0,1,0,8,0,100,2,45941,0,0,0,11,45939,0,0,0,0,0,1,0,0,0,0,0,0,0,'[PH] Ahune Loot Loc Bunny - On SpellHit - Cast \'Summon Loot\'');
+UPDATE `smart_scripts` SET `event_flags` = 4 WHERE `entryorguid` = 25746 AND `source_type` = 0 AND `id` = 0;
