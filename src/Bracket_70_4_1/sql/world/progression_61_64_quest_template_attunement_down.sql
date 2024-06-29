bb7105362ba88d791149670c6514f1a45c80d031
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2024-06-29 00:33:06 -0300

    fix: Move Hand of Adal title to WotLK release

--- src/Bracket_70_4_1/sql/world/progression_61_64_quest_template_attunement_down.sql
+++ src/Bracket_70_4_1/sql/world/progression_61_64_quest_template_attunement_down.sql
@@ -2,5 +2,4 @@
 UPDATE `quest_template` SET `RewardItem2` = 0, `RewardAmount2` = 0, `RewardTitle` = 0 WHERE (`ID` = 13430);
 -- Removes The Mark of Vashj spell to The Cudgel of Kar'desh (cosmetic)
 UPDATE `quest_template` SET `RewardSpell` = 0 WHERE (`ID` = 13431);
--- Remove Hand of A'dal Title reward for completing BT attunement
-UPDATE `quest_template` SET `RewardTitle` = 0 WHERE (`ID` = 10985);
+
