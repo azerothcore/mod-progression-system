79e7a1bb7d275925469b5cb483782b64ad81d229
Andrew <47818697+Nyeriah@users.noreply.github.com>
2024-07-04 21:32:41 -0300

    fix: move champion of naaru title (#406)

--- src/Bracket_70_4_1/sql/world/progression_61_64_quest_template_attunement_down.sql
+++ src/Bracket_70_4_1/sql/world/progression_61_64_quest_template_attunement_down.sql
@@ -1,5 +1,4 @@
--- Remove The Tempest Key and title from the rewards of Trial of the Naaru: Magtheridon
-UPDATE `quest_template` SET `RewardItem2` = 0, `RewardAmount2` = 0, `RewardTitle` = 0 WHERE (`ID` = 13430);
+-- Remove The Tempest Key from the rewards of Trial of the Naaru: Magtheridon
+UPDATE `quest_template` SET `RewardItem2` = 0, `RewardAmount2` = 0 WHERE (`ID` = 13430);
 -- Removes The Mark of Vashj spell to The Cudgel of Kar'desh (cosmetic)
 UPDATE `quest_template` SET `RewardSpell` = 0 WHERE (`ID` = 13431);
-
--- src/Bracket_80_1/sql/world/progression_61_64_quest_template_hand_of_adal_down.sql
+++ src/Bracket_80_1/sql/world/progression_61_64_quest_template_hand_of_adal_down.sql
@@ -1,2 +1,4 @@
 -- Remove Hand of A'dal Title reward for completing BT attunement
 UPDATE `quest_template` SET `RewardTitle` = 0 WHERE (`ID` = 10985);
+-- Remove title from the rewards of Trial of the Naaru: Magtheridon
+UPDATE `quest_template` SET `RewardTitle` = 0 WHERE (`ID` = 13430);
