8bb1686cfa0aafe51fd2cff52d4d80649031326a
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2025-11-10 16:50:13 +0000

    Added runed rings for icc bracket

---
+++ src/Bracket_80_4_1/sql/world/progression_80_4_1_runed_kirin_tor_rings.sql
@@ -0,0 +1,6 @@
+-- Adds Etched Band, Loop, Ring and Signet of Kirn Tor to Harold Winston with Icecrown Citadel Release
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
+(32172, 0, 51557, 0, 0, 2592, 0), -- Runed Signet of the Kirin Tor
+(32172, 0, 51558, 0, 0, 2593, 0), -- Runed Loop of the Kirin Tor
+(32172, 0, 51559, 0, 0, 2594, 0), -- Runed Ring of the Kirin Tor
+(32172, 0, 51560, 0, 0, 2595, 0); -- Runed Band of the Kirin Tor
