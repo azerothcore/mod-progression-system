cbea2042ef108b009ff28a0e21db8e06640de175
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2025-11-10 16:46:02 +0000

    Kirin tor Etched  rings with trial of the crusader

---
+++ src/Bracket_80_3/sql/world/progression_80_3_etched_kirin_tor_rings.sql
@@ -0,0 +1,6 @@
+-- Adds Etched Band, Loop, Ring and Signet of Kirn Tor to Harold Winston with Trial of the Crusader Release
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
+(32172, 0, 48954, 0, 0, 2592, 0), -- Etched Band of the Kirin Tor
+(32172, 0, 48955, 0, 0, 2593, 0), -- Etched Loop of the Kirin Tor
+(32172, 0, 48956, 0, 0, 2594, 0), -- Etched Ring of the Kirin Tor
+(32172, 0, 48957, 0, 0, 2595, 0); -- Etched Signet of the Kirin Tor
