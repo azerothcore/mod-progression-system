d059323063171aa9d2a94eb063e201240fb7ccf6
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2025-11-14 13:39:15 +0000

    Trial of the Crusader quest avaiable

--- src/Bracket_80_3/sql/world/progression_80_3_disables.sql
+++ src/Bracket_80_3/sql/world/progression_80_3_disables.sql
@@ -1,2 +1,6 @@
 -- 80 level range - Tier 9 (Call of the Crusade) & Relentless Gladiator
-DELETE FROM `disables` WHERE `entry` IN (249, 649);
+-- Makes instances (and RDF) Onyxia's Lair and Trial of the Crusader available again.
+DELETE FROM `disables` WHERE `sourceType` IN (2, 8) AND `entry` IN (249, 649);
+
+-- Make the quest Lord Jaraxxus Must Die! available again.
+DELETE FROM `disables` WHERE `sourceType` = 1 AND `entry` = 24589;
