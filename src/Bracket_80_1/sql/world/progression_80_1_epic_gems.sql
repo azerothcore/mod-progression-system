009fa5bdd54b935ae4360fbb53c935f9f0df3960
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-11-13 23:28:51 -0300

    Remove epic JC gems from vendors

---
+++ src/Bracket_80_1/sql/world/progression_80_1_epic_gems.sql
@@ -0,0 +1,17 @@
+DELETE FROM `disables` WHERE `sourceType` = 10 AND `entry` IN (36919, 36922, 36925, 36928, 36931, 36934);
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES
+(10, 36919, 0, 0, 0, 'Cardinal Ruby'),
+(10, 36922, 0, 0, 0, 'King\'s Amber'),
+(10, 36925, 0, 0, 0, 'Majestic Zircon'),
+(10, 36928, 0, 0, 0, 'Dreadstone'),
+(10, 36931, 0, 0, 0, 'Ametrine'),
+(10, 36934, 0, 0, 0, 'Eye of Zul');
+
+DELETE FROM `npc_vendor` WHERE `item` IN (36919, 36922, 36925, 36928, 36931, 36934);
+
+-- UPDATE `item_instance` SET `itemEntry` = 36918 WHERE `itemEntry` = 36919; -- Cardinal Ruby → Scarlet Ruby
+-- UPDATE `item_instance` SET `itemEntry` = 36921 WHERE `itemEntry` = 36922; -- King's Amber → Autumn's Glow
+-- UPDATE `item_instance` SET `itemEntry` = 36924 WHERE `itemEntry` = 36925; -- Majestic Zircon → Sky Sapphire
+-- UPDATE `item_instance` SET `itemEntry` = 36927 WHERE `itemEntry` = 36928; -- Dreadstone → Twilight Opal
+-- UPDATE `item_instance` SET `itemEntry` = 36930 WHERE `itemEntry` = 36931; -- Ametrine → Monarch Topaz
+-- UPDATE `item_instance` SET `itemEntry` = 36933 WHERE `itemEntry` = 36934; -- Eye of Zul → Forest Emerald
