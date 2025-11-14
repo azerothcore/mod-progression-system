24ea3069002cf399ccaba948499f929fab9672d2
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2025-11-14 14:36:30 +0000

    Adds back Proof of Demise: The Black Knight
    Implemented with the Call of the Crusade and gets removed with Fall of the Lich King

--- src/Bracket_80_3/sql/world/progression_80_3_disables.sql
+++ src/Bracket_80_3/sql/world/progression_80_3_disables.sql
@@ -1,6 +1,15 @@
 -- 80 level range - Tier 9 (Call of the Crusade) & Relentless Gladiator
--- Makes instances (and RDF) Onyxia's Lair and Trial of the Crusader available again.
-DELETE FROM `disables` WHERE `sourceType` IN (2, 8) AND `entry` IN (249, 649);
+-- Makes instances (and RDF) Onyxia's Lair, Trial of the Crusader and Trial of the Champion available again.
+DELETE FROM `disables` WHERE `sourceType` IN (2, 8) AND `entry` IN (249, 649, 650);
 
--- Make the quest Lord Jaraxxus Must Die! available again.
-DELETE FROM `disables` WHERE `sourceType` = 1 AND `entry` = 24589;
+-- Make the quests  Lord Jaraxxus Must Die! and Proof of Demise: The Black Knight available again.
+DELETE FROM `disables` WHERE `sourceType` = 1 AND `entry` IN (24589, 14199);
+
+-- Removes the flag UNAVAILABLE from Proof of Demise: The Black Knight
+UPDATE `quest_template` SET `flags` = `flags` &~ 16384 WHERE `ID` = 14199;
+-- Adds Archmage Lan'dalock as the quest starter
+DELETE FROM `creature_queststarter` WHERE `quest` = 14199 AND `id` = 20735;
+INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES (20735, 14199);
+-- Adds Archmage Lan'dalock as the quest ender
+DELETE FROM `creature_questender` WHERE `quest` = 14199 AND `id` = 20735;
+INSERT INTO `creature_questender` (`id`, `quest`) VALUES (20735, 14199);
