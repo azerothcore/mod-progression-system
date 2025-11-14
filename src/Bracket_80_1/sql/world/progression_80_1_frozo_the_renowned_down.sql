f5488ce5e879e689db10aeb3e384ca5a00885f18
Ryan Turner <16946913+TheSCREWEDSoftware@users.noreply.github.com>
2025-11-14 12:44:59 +0000

    Update progression_80_1_frozo_the_renowned_down.sql

--- src/Bracket_80_1/sql/world/progression_80_1_frozo_the_renowned_down.sql
+++ src/Bracket_80_1/sql/world/progression_80_1_frozo_the_renowned_down.sql
@@ -1,7 +1,6 @@
--- Adds back Frozo the Renowned and it's show.
-DELETE FROM `creature` WHERE `guid`=202846 AND `id1` = 40160;
-INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES (202846, 40160, 0, 0, 571, 4395, 4620, 1, 1, 1, 5910.08, 720.278, 643.425, 3.26377, 120, 0, 0, 8508, 7981, 0, 0, 0, 0, '', 0, 0, NULL);
-
+-- Restores Frozo's Phase back to 1 and his items
+UPDATE `creature` SET `phaseMask` = 1 WHERE `guid`=202846 AND `id1` = 40160;
+    
 DELETE FROM `npc_vendor` WHERE `entry` = 40160 AND `item` IN (35622, 35623, 35624, 35625, 35627, 36860, 36908, 45087, 47556, 54798);
 INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
 (40160, 0, 35622, 0, 0, 2985, 0), -- Eternal Water
