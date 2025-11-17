d3972594ff2a1e14892760088191a3ffce5aae46
Ryan Turner <16946913+TheSCREWEDSoftware@users.noreply.github.com>
2025-11-17 12:24:35 +0000

    Update progression_80_3_disables.sql

--- src/Bracket_80_3/sql/world/progression_80_3_disables.sql
+++ src/Bracket_80_3/sql/world/progression_80_3_disables.sql
@@ -2,5 +2,5 @@
 -- Makes instances (and RDF) Onyxia's Lair, Trial of the Crusader and Trial of the Champion available again.
 DELETE FROM `disables` WHERE `sourceType` IN (2, 8) AND `entry` IN (249, 649, 650);
 
--- Make the quests  Lord Jaraxxus Must Die! and Proof of Demise: The Black Knight available again.
-DELETE FROM `disables` WHERE `sourceType` = 1 AND `entry` IN (24589, 14199);
+-- Make the quests  Lord Jaraxxus Must Die! and available again.
+DELETE FROM `disables` WHERE `sourceType` = 1 AND `entry` = 24589;
