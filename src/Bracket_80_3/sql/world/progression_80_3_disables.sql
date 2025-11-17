51576632b8ad9f28d07f71a6beda6c102f1dcbfc
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2025-11-17 12:15:19 +0000

    Remove proof demise

--- src/Bracket_80_3/sql/world/progression_80_3_disables.sql
+++ src/Bracket_80_3/sql/world/progression_80_3_disables.sql
@@ -4,12 +4,3 @@ DELETE FROM `disables` WHERE `sourceType` IN (2, 8) AND `entry` IN (249, 649, 65
 
 -- Make the quests  Lord Jaraxxus Must Die! and Proof of Demise: The Black Knight available again.
 DELETE FROM `disables` WHERE `sourceType` = 1 AND `entry` IN (24589, 14199);
-
--- Removes the flag UNAVAILABLE from Proof of Demise: The Black Knight
-UPDATE `quest_template` SET `flags` = `flags` &~ 16384 WHERE `ID` = 14199;
--- Adds Archmage Lan'dalock as the quest starter
-DELETE FROM `creature_queststarter` WHERE `quest` = 14199 AND `id` = 20735;
-INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES (20735, 14199);
--- Adds Archmage Lan'dalock as the quest ender
-DELETE FROM `creature_questender` WHERE `quest` = 14199 AND `id` = 20735;
-INSERT INTO `creature_questender` (`id`, `quest`) VALUES (20735, 14199);
--- src/Bracket_80_4_1/sql/world/progression_80_4_1_disables.sql
+++ src/Bracket_80_4_1/sql/world/progression_80_4_1_disables.sql
@@ -4,10 +4,3 @@ DELETE FROM `disables` WHERE `sourceType` IN (2, 8) AND `entry` IN (631,632,658,
 
 -- Make the quests Inside the Frozen Citadel available again.
 DELETE FROM `disables` WHERE `sourceType` = 1 AND `entry` IN (24506, 24510);
-
--- Adds the flag UNAVAILABLE from Proof of Demise: The Black Knight
-UPDATE `quest_template` SET `flags` = `flags` | 16384 WHERE `ID` = 14199;
--- Removes Archmage Lan'dalock as the quest starter
-DELETE FROM `creature_queststarter` WHERE `quest` = 14199 AND `id` = 20735;
--- Removes Archmage Lan'dalock as the quest ender
-DELETE FROM `creature_questender` WHERE `quest` = 14199 AND `id` = 20735;
