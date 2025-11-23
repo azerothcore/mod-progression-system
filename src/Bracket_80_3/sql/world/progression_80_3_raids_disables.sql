4333990b5710d66495d27b32b0e8395f2917eab5
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-11-23 12:19:09 -0300

    fix query

--- src/Bracket_80_3/sql/world/progression_80_3_raids_disables.sql
+++ src/Bracket_80_3/sql/world/progression_80_3_raids_disables.sql
@@ -1,2 +1,2 @@
 -- Makes instances Naxxramas, EoE, OS, VOA available
-DELETE FROM `disables` WHERE `sourceType` IN 2 AND `entry` IN (533, 616, 615, 624);
+DELETE FROM `disables` WHERE `sourceType` = 2 AND `entry` IN (533, 616, 615, 624);
