7274289c57bc8391cf6b7ac9546feb0e932e43a7
Ryan Turner <16946913+TheSCREWEDSoftware@users.noreply.github.com>
2025-11-24 13:35:31 +0000

    Update progression_80_4_1_disables.sql

--- src/Bracket_80_4_1/sql/world/progression_80_4_1_disables.sql
+++ src/Bracket_80_4_1/sql/world/progression_80_4_1_disables.sql
@@ -6,4 +6,4 @@ DELETE FROM `disables` WHERE `sourceType` IN (2, 8) AND `entry` IN (631,632,658,
 DELETE FROM `disables` WHERE `sourceType` = 1 AND `entry` IN (24506, 24510);
 
 -- Makes the weekely gossip and quest/gossip flags for Archmage Lan'dalock
-UPDATE `creature_template` SET `gossip_menu_id` = 0, `npcflag` = `npcflag` | 3 WHERE `entry` = 20735;
+UPDATE `creature_template` SET `gossip_menu_id` = 10061, `npcflag` = `npcflag` | 3 WHERE `entry` = 20735;
