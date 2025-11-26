9657f27332b6fd6f97c55bcb7e27f423480917c8
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-11-26 20:28:36 -0300

    fix typo 2

--- src/Bracket_80_3/sql/world/progression_80_2_ArgentTournamentConstructionQuests_down.sql
+++ src/Bracket_80_3/sql/world/progression_80_2_ArgentTournamentConstructionQuests_down.sql
@@ -1,4 +1,4 @@
-UPDATE `creature_template` SET `npcflag` = `npcflag`~2 WHERE `entry` IN (33434, 33435);
+UPDATE `creature_template` SET `npcflag` = `npcflag`&~2 WHERE `entry` IN (33434, 33435);
 
 DELETE FROM `creature_queststarter` WHERE `quest` IN (13820, 13681, 13627);
 
