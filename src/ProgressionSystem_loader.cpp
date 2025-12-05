cef629bf6b3d39c35725e20bcebcb357ba219583
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-12-04 21:39:42 -0300

    Add AddBracket_80_1_B_Scripts to loader
    Introduces the AddBracket_80_1_B_Scripts function declaration and ensures it is called within Addmod_progression_systemScripts. This allows scripts for the 80_1_B bracket to be properly loaded.

--- src/ProgressionSystem_loader.cpp
+++ src/ProgressionSystem_loader.cpp
@@ -37,6 +37,7 @@ void AddBracket_70_6_C_Scripts();
 void AddBracket_71_74_Scripts();
 void AddBracket_75_79_Scripts();
 void AddBracket_80_1_Scripts();
+void AddBracket_80_1_B_Scripts();
 void AddBracket_80_2_Scripts();
 void AddBracket_80_3_Scripts();
 void AddBracket_80_4_A_Scripts();
@@ -82,6 +83,7 @@ void Addmod_progression_systemScripts()
     AddBracket_71_74_Scripts();
     AddBracket_75_79_Scripts();
     AddBracket_80_1_Scripts();
+    AddBracket_80_1_B_Scripts();
     AddBracket_80_2_Scripts();
     AddBracket_80_3_Scripts();
     AddBracket_80_4_A_Scripts();
