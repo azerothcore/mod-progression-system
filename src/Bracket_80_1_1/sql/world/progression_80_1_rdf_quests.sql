369e79b29d7d09486ff0f8c05c673fbdc1df6734
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-12-04 21:44:12 -0300

    Set RewardAmount1 for quest 24788 in RDF quests SQL
    Added `RewardAmount1 = 1` to the quest with ID 24788 to specify the reward amount for the daily first Valor (heroic) quest.

--- src/Bracket_80_1_1/sql/world/progression_80_1_rdf_quests.sql
+++ src/Bracket_80_1_1/sql/world/progression_80_1_rdf_quests.sql
@@ -1,3 +1,3 @@
 UPDATE `quest_template` SET `rewarditem1` = 40752 WHERE `ID` = 24790; -- Daily Nth, Heroism (normal)
 UPDATE `quest_template` SET `rewarditem1` = 40752 WHERE `ID` = 24789; -- Daily Nth, Heroism (heroic)
-UPDATE `quest_template` SET `rewarditem1` = 40753 WHERE `ID` = 24788; -- Daily 1st, Valor (heroic)
+UPDATE `quest_template` SET `rewarditem1` = 40753, `RewardAmount1` = 1 WHERE `ID` = 24788; -- Daily 1st, Valor (heroic)
