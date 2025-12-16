d5ab0468e44e28b07b947df8520ec8fa009f16f7
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-12-16 19:29:16 -0300

    Refactor PVP vendor SQL and add down migration
    Removed redundant INSERT statements from progression_80_1_2_pvp_vendors.sql and added a corresponding down migration script in Bracket_80_2. This improves migration management and separation of up/down SQL logic.

--- src/Bracket_80_2/sql/world/progression_80_1_2_pvp_vendors_down.sql
+++ src/Bracket_80_2/sql/world/progression_80_1_2_pvp_vendors_down.sql
@@ -1,2 +1,4 @@
 DELETE FROM `game_event_creature` WHERE `guid` IN (152028, 152031) AND `eventEntry` = 57;
-
+INSERT INTO `game_event_creature` (`eventEntry`, `guid`) VALUES
+(57,152028),
+(57,152031);
