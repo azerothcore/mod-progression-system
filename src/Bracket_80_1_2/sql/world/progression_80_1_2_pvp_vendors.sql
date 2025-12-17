dc343c8cad2f4510cc9aaa4fc852eedee0d5a59b
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-12-17 05:08:52 -0300

    Phase out pvp vendors

--- src/Bracket_80_1_2/sql/world/progression_80_1_2_pvp_vendors.sql
+++ src/Bracket_80_1_2/sql/world/progression_80_1_2_pvp_vendors.sql
@@ -1,3 +1,3 @@
 DELETE FROM `game_event_creature` WHERE `guid` IN (152028, 152031) AND `eventEntry` = 57;
-UPDATE `creature` SET `phaseMask` = 2 WHERE `guid` IN (133920, 125693);
+UPDATE `creature` SET `phaseMask` = 2 WHERE `guid` IN (133920, 125693, 125691, 133917);
 
