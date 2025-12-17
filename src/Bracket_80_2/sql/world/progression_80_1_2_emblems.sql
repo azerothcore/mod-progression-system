a9b7f3e44760427c5586f79168add9cc4b7ef9ec
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-12-17 05:05:59 -0300

    Switch Emblem of Triumph for Heroism

---
+++ src/Bracket_80_2/sql/world/progression_80_1_2_emblems.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_loot_template` SET `Item` = 40752 WHERE `Item` = 47241;
+UPDATE `gameobject_loot_template` SET `Item` = 40752 WHERE `Item` = 47241;
+UPDATE `reference_loot_template` SET `Item` = 40752 WHERE `Item` = 47241;
