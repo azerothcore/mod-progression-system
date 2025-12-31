4e897f7f40fe8ea819782448851c9d53f5afd2b3
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-12-31 06:23:59 -0300

    Update progression_61_64_npc_trainer_TBCprofs_down_2.sql

--- src/Bracket_70_6_1/sql/world/progression_61_64_npc_trainer_TBCprofs_down_2.sql
+++ src/Bracket_70_6_1/sql/world/progression_61_64_npc_trainer_TBCprofs_down_2.sql
@@ -1,4 +1,4 @@
 -- Restore Brilliant Glass for Sunwell
-DELETE FROM `npc_trainer` WHERE `SpellID`=47280;
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES
-(201025, 47280, 30000, 755, 350, 0, 0);
+-- DELETE FROM `npc_trainer` WHERE `SpellID`=47280;
+-- INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES
+-- (201025, 47280, 30000, 755, 350, 0, 0);
