ddf48e886cc18a7d682530a27086f6fb39b28234
Andrew <47818697+Nyeriah@users.noreply.github.com>
2026-04-14 22:23:57 -0300

    feat: OS satchels (#471)

--- src/Bracket_80_2/sql/world/progression_80_1_3_obsidian_sanctum_emblems_down.sql
+++ src/Bracket_80_2/sql/world/progression_80_1_3_obsidian_sanctum_emblems_down.sql
@@ -12,3 +12,15 @@ UPDATE `creature_loot_template`
 SET `Item` = 47241, `Comment` = 'Emblem of Triumph'
 WHERE `Entry` = 28860
 AND `Item` = 40753;
+
+-- Satchel of Spoils (10M): Valor -> Triumph
+UPDATE `item_loot_template`
+SET `Item` = 47241, `Comment` = 'Satchel of Spoils - Emblem of Triumph'
+WHERE `Entry` = 43347
+AND `Item` = 40753;
+
+-- Large Satchel of Spoils (25M): Valor -> Triumph
+UPDATE `item_loot_template`
+SET `Item` = 47241, `Comment` = 'Large Satchel of Spoils - Emblem of Triumph'
+WHERE `Entry` = 43346
+AND `Item` = 40753;
