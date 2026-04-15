3ae355aa5de40d989dd508282b5a3d8c72c07eee
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2026-04-14 21:34:58 -0300

    feat: update OS emblems

---
+++ src/Bracket_80_2/sql/world/progression_80_1_3_obsidian_sanctum_emblems_down.sql
@@ -0,0 +1,14 @@
+-- Obsidian Sanctum drakes: Valor -> Triumph
+-- Shadron (10N: 30451, 25N: 31520)
+-- Vesperon (10N: 30449, 25N: 31535)
+-- Tenebron (10N: 30452, 25N: 31534)
+UPDATE `creature_loot_template`
+SET `Item` = 47241, `Comment` = 'Emblem of Triumph'
+WHERE `Entry` IN (30451, 31520, 30449, 31535, 30452, 31534)
+AND `Item` = 40753;
+
+-- Sartharion 10M: Valor -> Triumph
+UPDATE `creature_loot_template`
+SET `Item` = 47241, `Comment` = 'Emblem of Triumph'
+WHERE `Entry` = 28860
+AND `Item` = 40753;
